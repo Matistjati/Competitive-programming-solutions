f5be42ab25d0f33960f746bc5f17ff3d069ae3ad
Matistjati <matistjati@outlook.com>
2021-10-15 19:32:47 +0200

    Solved buying votes

---
+++ po/rostkop.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <sstream>
+#include <algorithm>
+#include <cmath>
+#include <set>
+#include <unordered_set>
+#include <string>
+#include <iterator>
+#include <queue>
+#include <tuple>
+#include <numeric>
+#include <random>
+#include <time.h>
+#include <stack>
+#include <chrono>
+#include <unordered_map>
+
+using namespace std;
+
+#define ll long long
+#define p2 pair<ll, ll>
+#define p3 tuple<ll, ll, ll>
+#define vi vector<ll>
+#define inf 1e9
+
+#define read(a) cin >> a
+#define write(a) cout << (a) << endl
+#define deb nothing();
+
+#define readpush(type,a) type temp; read(temp); a.push_back(temp)
+#define readinsert(type,a) type temp; read(temp); a.insert(temp)
+#define setcontains(set, x) (set.find(x) != set.end())
+#define all(a) begin(a),end(a)
+
+#define rep(i, high) for (ll i = 0; i < high; i++)
+#define repe(i, container) for (auto& i : container)
+#define per(i, high) for (ll i = high; i >= 0; i--)
+
+#define ceildiv(x,y) ((x + y - 1) / y)
+
+inline void nothing() {}
+
+inline void fast()
+{
+    ios::sync_with_stdio(false);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+}
+
+int main()
+{
+    fast();
+
+    int n;
+    int gunnar;
+    read(n);
+    read(gunnar);
+    priority_queue<int> votes;
+    rep(i, n-1)
+    {
+        int temp;
+        read(temp);
+        votes.push(temp);
+    }
+
+    int ans = 0;
+
+    while (gunnar <= votes.top())
+    {
+        int most = votes.top();
+        most--;
+        votes.pop();
+        votes.push(most);
+
+        gunnar++;
+        ans++;
+    }
+
+    write(ans);
+
+    return 0;
+}
