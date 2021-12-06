ead72488db2f43d06489ff2f41ffe571a75ecfbe
Matistjati <matistjati@outlook.com>
2021-12-06 22:03:29 +0100

    Got shared first place on summarum

--- po/summarum.cpp
+++ po/summarum.cpp
@@ -1,24 +1,10 @@
-#include <iostream>
-#include <vector>
-#include <map>
-#include <sstream>
-#include <algorithm>
-#include <cmath>
-#include <set>
-#include <unordered_set>
-#include <string>
-#include <iterator>
-#include <queue>
-#include <tuple>
-#include <numeric>
-#include <random>
-#include <time.h>
-#include <stack>
-#include <chrono>
-#include <unordered_map>
-#include <iomanip>
+#include <bits/stdc++.h>
 
 using namespace std;
+
+#pragma GCC target ("avx2")
+#pragma GCC optimization ("O3")
+#pragma GCC optimization ("unroll-loops")
 
 #define ll long long
 #define vi vector<ll>
@@ -26,29 +12,47 @@ using namespace std;
 #define p2 pair<ll, ll>
 #define p3 vi
 #define p4 vi
+#define ip3 tuple<int,int,int>
+#define ip4 tuple<int,int,int,int>
+#define vp2 vector<p2>
+#define vp3 vector<p3>
 #define inf 2e9
 #define linf 1e17
 
 #define read(a) cin >> a
-#define write(a) cout << (a) << endl
+#define write(a) cout << (a) << "\n"
+#define dread(type, a) type a; cin >> a
+#define dread2(type, a, b) dread(type, a); dread(type, b)
+#define dread3(type, a, b, c) dread2(type, a, b); dread(type, c)
+#define dread4(type, a, b, c, d) dread3(type, a, b, c); dread(type, d)
+#define dread5(type, a, b, c, d, e) dread4(type, a, b, c, d); dread(type, e)
+#ifdef _DEBUG
 #define deb __debugbreak();
-
-#define readpush(type,a) type temp; read(temp); a.push_back(temp)
-#define readinsert(type,a) type temp; read(temp); a.insert(temp)
-#define setcontains(set, x) (set.find(x) != set.end())
-#define all(a) begin(a),end(a)
+#else
+#define deb ;
+#endif
 
 #define rep(i, high) for (ll i = 0; i < high; i++)
 #define repe(i, container) for (auto& i : container)
 #define per(i, high) for (ll i = high; i >= 0; i--)
 
-#define ceildiv(x,y) ((x + y - 1) / y)
+#define readpush(type,vect) type temp; read(temp); vect.push_back(temp);
+#define readvector(type, name, size) vector<type> name(size); rep(i,size) {dread(type,temp); name[i]=temp;}
+#define readinsert(type,a) {type temp; read(temp); a.insert(temp);}
+#define all(a) begin(a),end(a)
+#define setcontains(set, x) (set.find(x) != set.end())
+#define stringcontains(str, x) (str.find(x) != string::npos)
+#define within(a, b, c, d) (a >= 0 && a < b && c >= 0 && c < d)
 
+#define ceildiv(x,y) ((x + y - 1) / y)
+#define fract(a) (a-floor(a))
+#define sign(a) ((a>0) ? 1 : -1)
+
+auto Start = chrono::high_resolution_clock::now();
 
 inline void fast()
 {
     ios::sync_with_stdio(false);
-    ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
 }
 
@@ -56,41 +60,51 @@ int main()
 {
     fast();
 
+#if 0
+    ifstream cin("C:\\Users\\Matis\\source\\repos\\Comp prog\\x64\\Debug\\in.txt");
+#endif
+
     int n;
     read(n);
-    vi an;
-    vi be;
+    vector<int> an(n);
+    vector<int> be(n);
+    ll sumAn = 0;
+    ll sumBe = 0;
     rep(i, n)
     {
-        readpush(ll, an);
+        dread(int, a);
+        sumAn += a;
+        an[i] = a;
     }
     rep(i, n)
     {
-        readpush(ll, be);
+        dread(int, b);
+        sumBe += b;
+        be[i] = b;
     }
 
-    ll sumAn = accumulate(all(an), 0LL);
-    ll sumBe = accumulate(all(be), 0LL);
-    sort(all(an));
     sort(all(be));
 
     ll ans = linf;
 
+    ll diff = sumAn - sumBe;
+
     rep(i, n)
     {
-        ll outer = -(sumAn - sumBe - 2 * an[i]) / 2;
+        ll outer = -(diff - 2 * an[i]) << 1;
         auto bestMatch = lower_bound(all(be), outer);
         if (bestMatch != be.end())
         {
-            ans = min(ans, abs(sumAn - sumBe - 2 * an[i] + 2 * (*bestMatch)));
+            ans = min(ans, abs(diff - 2 * an[i] + 2 * (*bestMatch)));
         }
         else
         {
-            ans = min(ans, abs(sumAn - sumBe - 2 * an[i] + 2 * *prev(bestMatch)));
+            ans = min(ans, abs(diff - 2 * an[i] + 2 * *prev(bestMatch)));
         }
     }
 
     write(ans);
 
-    return 0;
+    cout << endl;
+    _Exit(0);
 }
