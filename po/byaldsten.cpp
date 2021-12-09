9766d932311c268616af73a4422bd671a1bafa15
Matistjati <matistjati@outlook.com>
2021-12-09 13:48:19 +0100

    Got first place on byäldsten

--- po/byaldsten.cpp
+++ po/byaldsten.cpp
@@ -1,51 +1,59 @@
-#include <iostream>
-#include <vector>
-#include <map>
-#include <sstream>
-#include <algorithm>
-#include <cmath>
-#include <set>
-#include <unordered_set>
-#include <string>
-#include <iterator>
-#include <queue>
-#include <tuple>
-#include <numeric>
-#include <random>
-#include <time.h>
-#include <stack>
-#include <chrono>
-#include <unordered_map>
+#include <bits/stdc++.h>
 
 using namespace std;
 
+#pragma GCC target ("avx2")
+#pragma GCC optimization ("O3")
+#pragma GCC optimization ("unroll-loops")
+
 #define ll long long
-#define p2 pair<ll, ll>
-#define p3 vector<ll>
-#define p4 vector<ll>
 #define vi vector<ll>
-#define inf 1e9
+#define vvi vector<vi>
+#define p2 pair<ll, ll>
+#define p3 tuple<ll,ll,ll>
+#define p4 vi
+#define ip3 tuple<int,int,int>
+#define ip4 tuple<int,int,int,int>
+#define vp2 vector<p2>
+#define vp3 vector<p3>
+#define inf 2e9
+#define linf 1e17
 
 #define read(a) cin >> a
-#define write(a) cout << (a) << endl
+#define write(a) cout << (a) << "\n"
+#define dread(type, a) type a; cin >> a
+#define dread2(type, a, b) dread(type, a); dread(type, b)
+#define dread3(type, a, b, c) dread2(type, a, b); dread(type, c)
+#define dread4(type, a, b, c, d) dread3(type, a, b, c); dread(type, d)
+#define dread5(type, a, b, c, d, e) dread4(type, a, b, c, d); dread(type, e)
+#ifdef _DEBUG
 #define deb __debugbreak();
-
-#define readpush(type,a) type temp; read(temp); a.push_back(temp)
-#define readinsert(type,a) type temp; read(temp); a.insert(temp)
-#define setcontains(set, x) (set.find(x) != set.end())
-#define all(a) begin(a),end(a)
+#else
+#define deb ;
+#endif
 
 #define rep(i, high) for (ll i = 0; i < high; i++)
+#define repp(i, low, high) for (ll i = low; i < high; i++)
 #define repe(i, container) for (auto& i : container)
 #define per(i, high) for (ll i = high; i >= 0; i--)
 
-#define ceildiv(x,y) ((x + y - 1) / y)
+#define readpush(type,vect) type temp; read(temp); vect.push_back(temp);
+#define readvector(type, name, size) vector<type> name(size); rep(i,size) {dread(type,temp); name[i]=temp;}
+#define readinsert(type,a) {type temp; read(temp); a.insert(temp);}
+#define all(a) begin(a),end(a)
+#define setcontains(set, x) (set.find(x) != set.end())
+#define stringcontains(str, x) (str.find(x) != string::npos)
+#define within(a, b, c, d) (a >= 0 && a < b && c >= 0 && c < d)
 
+#define ceildiv(x,y) ((x + y - 1) / y)
+#define fract(a) (a-floor(a))
+#define sign(a) ((a>0) ? 1 : -1)
+
+auto Start = chrono::high_resolution_clock::now();
 
 inline void fast()
 {
     ios::sync_with_stdio(false);
-    ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
 }
 
@@ -53,33 +61,39 @@ int main()
 {
     fast();
 
+#if 0
+    ifstream cin("C:\\Users\\Matis\\source\\repos\\Comp prog\\x64\\Debug\\in.txt");
+#endif
+
     int n;
     read(n);
 
-    vector<p3> people;
+    vector<tuple<int,int,int>> people(n);
     rep(i, n)
     {
         int born;
         int death;
         read(born);
         read(death);
-        people.push_back({ born,death, i });
+        people[i] = {born,death, i};
     }
 
     sort(all(people));
     vector<int> n_speeches(n);
 
 
-    p3 prevOldest = {0,0,-1};
+    p3 prevOldest = { 0,0,-1 };
     ll currOldest = 0;
     rep(i, n)
     {
-        p3 newPerson = people[i];
-        currOldest = max(currOldest, newPerson[1]);
-        if (newPerson[1] > prevOldest[1])
+        int born, death, index;
+        tie(born, death, index) = people[i];
+
+        currOldest = max(currOldest, (ll)death);
+        if (death > get<1>(prevOldest))
         {
-            n_speeches[newPerson[2]] += min(newPerson[1] - prevOldest[1], newPerson[1] - newPerson[0]);
-            prevOldest = newPerson;
+            n_speeches[index] += min(death - get<1>(prevOldest), (ll) death - born);
+            prevOldest = people[i];
         }
 
 
@@ -90,5 +104,6 @@ int main()
         write(n_speeches[i]);
     }
 
-    return 0;
+    cout << endl;
+    _Exit(0);
 }
