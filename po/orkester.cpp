fd14ddcebbd3b772377f66da92ef19faf0f2619e
Matistjati <matistjati@outlook.com>
2022-01-09 04:25:54 +0100

    Solved orkesteroptimering

---
+++ po/orkester.cpp
@@ -0,0 +1,171 @@
+#include <bits/stdc++.h>
+//#include <bits/extc++.h>
+
+using namespace std;
+
+#pragma GCC target ("avx2")
+#pragma GCC optimization ("O3")
+#pragma GCC optimization ("unroll-loops")
+
+#define enablell 0
+
+#define ll long long
+#if enablell
+#define int ll
+#endif
+#define vi vector<int>
+#define vb vector<bool>
+#define vvb vector<vb>
+#define vvvb vector<vvb>
+#define vvi vector<vi>
+#define vvvi vector<vvi>
+#define vvvvi vector<vvvi>
+#define p2 pair<int, int>
+#define p3 tuple<int,int,int>
+#define p4 tuple<int,int,int,int>
+#define vp2 vector<p2>
+#define vvp2 vector<vp2>
+#define vp3 vector<p3>
+#define vp4 vector<p4>
+#if enablell
+#define inf 1e18
+#else
+#define inf int(1e9)
+#endif
+
+#define read(a) cin >> a
+#define read2(a,b) cin >> a >> b
+#define write(a) cout << (a) << "\n"
+#define quit cout << endl; _Exit(0);
+#define dread(type, a) type a; cin >> a
+#define dread2(type, a, b) dread(type, a); dread(type, b)
+#define dread3(type, a, b, c) dread2(type, a, b); dread(type, c)
+#define dread4(type, a, b, c, d) dread3(type, a, b, c); dread(type, d)
+#define dread5(type, a, b, c, d, e) dread4(type, a, b, c, d); dread(type, e)
+#ifdef _DEBUG
+#define noop cout << "";
+#define deb __debugbreak();
+#else
+#define noop ;
+#define deb ;
+#endif
+
+#define rep(i, high) for (int i = 0; i < high; i++)
+#define repp(i, low, high) for (int i = low; i < high; i++)
+#define repe(i, container) for (auto& i : container)
+#define per(i, high) for (int i = high; i >= 0; i--)
+#define perr(i, low, high) for (int i = high; i >= low; i--)
+
+#define readpush(type,vect) type temp; read(temp); vect.(temp);
+#define readvector(type, name, size) vector<type> name(size); rep(i,size) {dread(type,temp); name[i]=temp;}
+#define readinsert(type,a) {type temp; read(temp); a.insert(temp);}
+#define all(a) begin(a),end(a)
+#define setcontains(set, x) (set.find(x) != set.end())
+#define stringcontains(str, x) (str.find(x) != string::npos)
+#define within(a, b, c, d) (a >= 0 && a < b && c >= 0 && c < d)
+#define insideinterval(v, interval) (v >= interval.first && v <= interval.second)
+#define sz(container) ((int)container.size())
+#define mp(a,b) (make_pair(a,b))
+#define first(a) (*begin(a))
+
+#define ceildiv(x,y) ((x + y - 1) / y)
+#define fract(a) (a-floor(a))
+#define sign(a) ((a>0) ? 1 : -1)
+
+auto Start = chrono::high_resolution_clock::now();
+
+inline void fast()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+}
+
+template <typename T, typename U>
+void operator+=(std::pair<T, U>& l, const std::pair<T, U>& r)
+{
+    l = { l.first + r.first,l.second + r.second };
+}
+
+bool match(vvi& musicians, int u, vb& seen, vi& matchedWith)
+{
+    if (matchedWith[u] == -1) return true;
+
+    int v = matchedWith[u];
+    if (seen[v]) return false;
+    seen[v] = true;
+
+    repe(measure, musicians[v])
+    {
+        if (match(musicians, measure, seen, matchedWith))
+        {
+            matchedWith[measure] = v;
+            return true;
+        }
+    }
+
+    return false;
+}
+
+
+int32_t main()
+{
+    fast();
+
+#if 0
+    ifstream cin("C:\\Users\\Matis\\source\\repos\\Comp prog\\x64\\Release\\in.txt");
+#endif
+
+    dread2(int, n, m);
+
+    vvi musicians(n);
+
+
+    rep(i, n)
+    {
+        dread(int, t);
+
+        rep(j, t)
+        {
+            dread(int, measure);
+            measure--;
+            musicians[i].emplace_back(measure);
+        }
+    }
+
+
+    double ans = 0;
+
+    queue<p2> toMatch;
+    rep(i, n)
+    {
+        toMatch.emplace(i, 1);
+    }
+
+    vi matchedWith(m, -1);
+
+    vb seen(n);
+
+    while (toMatch.size())
+    {
+        p2 curr = toMatch.front();
+        toMatch.pop();
+
+        fill(all(seen), false);
+        seen[curr.first] = true;
+
+        repe(i, musicians[curr.first])
+        {
+            if (match(musicians, i, seen, matchedWith))
+            {
+                ans += 1.0 / curr.second;
+                matchedWith[i] = curr.first;
+                toMatch.emplace(curr.first, curr.second + 1);
+                break;
+            }
+        }
+    }
+
+    cout << fixed << setprecision(15) << ans;
+
+    quit;
+}
