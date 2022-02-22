141b40ff419bfc69733d620e1be6d57ed355159c
Matistjati <matistjati@outlook.com>
2022-02-22 03:40:42 +0100

    Solved where's waldo for 99 points

---
+++ pocamp22/whereswaldo.cpp
@@ -0,0 +1,172 @@
+#undef _GLIBCXX_DEBUG                // disable run-time bound checking, etc
+#pragma GCC optimize("Ofast,inline") // Ofast = O3,fast-math,allow-store-data-races,no-protect-parens
+#pragma GCC optimize ("unroll-loops")
+
+#pragma GCC target("bmi,bmi2,lzcnt,popcnt")                      // bit manipulation
+#pragma GCC target("movbe")                                      // byte swap
+#pragma GCC target("aes,pclmul,rdrnd")                           // encryption
+#pragma GCC target("avx,avx2,f16c,fma,sse3,ssse3,sse4.1,sse4.2") // SIMD
+
+#include <bits/stdc++.h>
+//#include <bits/extc++.h>
+
+using namespace std;
+
+#define enablell 1
+
+#define ll long long
+#if enablell
+#define int ll
+#define inf 2e18
+#define float double
+#else
+#define inf int(2e9)
+#endif
+#define vi vector<int>
+#define vvi vector<vi>
+#define vvvi vector<vvi>
+#define vvvvi vector<vvvi>
+#define vb vector<bool>
+#define vvb vector<vb>
+#define vvvb vector<vvb>
+#define p2 pair<int, int>
+#define vp2 vector<p2>
+#define vvp2 vector<vp2>
+#define vvvp2 vector<vvp2>
+#define p3 tuple<int,int,int>
+#define vp3 vector<p3>
+#define vvp3 vector<vp3>
+#define vvvp3 vector<vvp3>
+#define p4 tuple<int,int,int,int>
+#define vp4 vector<p4>
+
+#define read(a) cin >> a
+#define read2(a,b) cin >> a >> b
+#define read3(a,b,c) cin >> a >> b >> c
+#define write(a) cout << (a) << "\n"
+#define quit cout << endl; _Exit(0);
+#define dread(type, a) type a; cin >> a
+#define dread2(type, a, b) dread(type, a); dread(type, b)
+#define dread3(type, a, b, c) dread2(type, a, b); dread(type, c)
+#define dread4(type, a, b, c, d) dread3(type, a, b, c); dread(type, d)
+#define dread5(type, a, b, c, d, e) dread4(type, a, b, c, d); dread(type, e)
+#ifdef _DEBUG
+#define noop cout << "";
+#define deb __debugbreak();
+#define debassert(expr) if (!(expr)) deb;
+#define debif(expr) if(expr) deb;
+#else
+#define noop ;
+#define deb ;
+#define debassert(expr) ;
+#define debif(expr) ;
+#endif
+
+#define rep(i, high) for (int i = 0; i < high; i++)
+#define repp(i, low, high) for (int i = low; i < high; i++)
+#define repe(i, container) for (auto& i : container)
+#define per(i, high) for (int i = high-1; i >= 0; i--)
+#define perr(i, low, high) for (int i = high-1; i >= low; i--)
+
+#define readvector(type, name, size) vector<type> name(size); rep(i,size) {dread(type,temp); name[i]=temp;}
+#define all(a) begin(a),end(a)
+#define setcontains(set, x) (set.find(x) != set.end())
+#define stringcontains(str, x) (str.find(x) != string::npos)
+#define within(a, b, c, d) (a >= 0 && a < b && c >= 0 && c < d)
+#define sz(container) ((int)container.size())
+#define mp(a,b) (make_pair(a,b))
+#define first(a) (*begin(a))
+
+#define ceildiv(x,y) ((x + y - 1) / y)
+#define fract(a) (a-floor(a))
+
+auto Start = chrono::high_resolution_clock::now();
+#define elapsedmillis() (chrono::duration_cast<chrono::milliseconds>(chrono::high_resolution_clock::now() - Start).count())
+#define rununtil(time) if (elapsedmillis() >= time) break;
+
+inline void fast() { ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); }
+template <typename T, typename U> inline void operator+=(std::pair<T, U>& l, const std::pair<T, U>& r) { l = { l.first + r.first,l.second + r.second }; }
+template <typename T> inline int sgn(T val) { return (T(0) < val) - (val < T(0)); }
+template <typename Out> inline void split(const string& s, char delim, Out result) { istringstream iss(s);  string item; while (getline(iss, item, delim)) { *result++ = item; } }
+inline vector<string> split(const string& s, char delim) { vector<string> elems; split(s, delim, back_inserter(elems)); return elems; }
+
+int32_t main()
+{
+    fast();
+
+#if 0
+    ifstream cin("C:\\Users\\Matis\\source\\repos\\Comp prog\\x64\\Debug\\in.txt");
+#endif
+
+    dread2(int, t, n);
+
+    vi tryfirst = { 5 };
+
+    vp2 priorities = {  };
+    rep(i, n+1)
+    {
+        priorities.emplace_back(0, 0);
+    }
+
+    rep(i, t)
+    {
+        priority_queue<tuple<double, int, int, int>> goodintervals;
+
+
+        goodintervals.emplace(-(n * (n + 1) / 2) / (n + 0.), (n * (n + 1) / 2), 1, n);
+
+        int interval = 0;
+        while (goodintervals.size())
+        {
+            int score, start, fin;
+            double average;
+            tie(average, score, start, fin) = goodintervals.top();
+            goodintervals.pop();
+
+            int maxSum = 1;
+            int k = 0;
+            repp(j, start, fin)
+            {
+                maxSum += n - k;
+                k++;
+            }
+
+            if (score>maxSum)
+            {
+                continue;
+            }
+
+            int mid = start + (fin - start) / 2;
+            cout << "? " << start << " " << mid << "\n" << flush;
+            dread(int, num);
+            int rightScore = score - num;
+            if (num == 1)
+            {
+                cout << "! " << start << "\n" << flush;
+                break;
+            }
+            else if (rightScore == 1)
+            {
+                cout << "! " << mid + 1 << "\n" << flush;
+                break;
+            }
+            const double d = 1.049500001; // Priorotize small intervals
+
+            if (start != mid)
+            {
+                double heuristic = -double(num) / pow(mid - start + 1.0, d);
+                goodintervals.emplace(heuristic, num, start, mid);
+            }
+            if (mid + 1 != fin)
+            {
+                double heuristic = -double(rightScore) / pow(fin - mid + 0., d);
+                goodintervals.emplace(heuristic, rightScore, mid + 1, fin);
+            }
+            interval++;
+        }
+    nexttc:;
+
+    }
+
+    quit;
+}
