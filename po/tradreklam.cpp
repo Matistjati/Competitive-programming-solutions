f174e86d57153bfef970ce2d81ba5cd33c683504
Matistjati <matistjati@outlook.com>
2022-05-10 21:21:21 +0200

    Solved trädreklam

--- po/tradreklam.cpp
+++ po/tradreklam.cpp
@@ -12,7 +12,7 @@
 
 using namespace std;
 
-#define enablell 1
+#define enablell 0
 
 #define ll long long
 #if enablell
@@ -93,6 +93,52 @@ template <typename T> inline int sgn(T val) { return (T(0) < val) - (val < T(0))
 template <typename Out> inline void split(const string& s, char delim, Out result) { istringstream iss(s); string item; while (getline(iss, item, delim)) { *result++ = item; } }
 inline vector<string> split(const string& s, char delim) { vector<string> elems; split(s, delim, back_inserter(elems)); return elems; }
 
+int fillcities(vp2& cities, vvp2& edges, vi& populations, int node, int par)
+{
+    int ret = 0;
+
+    repe(edge, edges[node])
+    {
+        if (edge.first == par) continue;
+        cities[edge.first].first = edge.second;
+        ret += fillcities(cities, edges, populations, edge.first, node);
+    }
+
+    ret += populations[node];
+
+    cities[node].second = ret;
+
+    return ret;
+}
+
+// Cities: first cost, second value
+vi best(vp2& cities, vvp2& edges, vi dp, int node, int par)
+{
+    vi dp2(dp.size(), 0);
+
+    rep(i, dp.size())
+    {
+        int newV = 0;
+        if (cities[node].first <= i)
+        {
+            newV = cities[node].second + dp[i - cities[node].first];
+        }
+
+        dp2[i] = max(dp[i], newV);
+    }
+
+    repe(edge, edges[node])
+    {
+        if (edge.first == par) continue;
+
+        dp = best(cities, edges, dp, edge.first, node);
+    }
+
+    rep(i, dp.size()) dp2[i] = max(dp2[i], dp[i]);
+
+    return dp2;
+}
+
 int32_t main()
 {
     fast();
@@ -101,35 +147,33 @@ int32_t main()
     ifstream cin("C:\\Users\\Matis\\source\\repos\\Comp prog\\x64\\Debug\\in.txt");
 #endif
 
+    dread2(int, n, b);
 
-    dread(int, t);
+    vvp2 edges(n);
 
-    int tc = 1;
+    vi populations(n);
+    rep(i, n-1) cin >> populations[i + 1];
 
-    while (t--)
+    rep(i, n - 1)
     {
-
-
-        dread(int, n);
-        readvector(int, dices, n);
-
-        sort(all(dices));
-        int ans = 0;
-
-        rep(i, n)
-        {
-            if (dices[i] < ans+1)
-            {
-                continue;
-            }
-            ans++;
-        }
-
-        cout << "Case #" << tc << ": " << ans << "\n";
-
-        tc++;
+        dread3(int, a, b, c);
+        a--;
+        b--;
+        edges[a].emplace_back(b, c);
+        edges[b].emplace_back(a, c);
     }
 
+
+    // Cost, value
+    vp2 cities(n);
+    fillcities(cities, edges, populations, 0, -1);
+    cities[0].first = inf;
+
+    vi dp(b+1);
+
+    vi ans = best(cities, edges, dp, 0, -1);
+
+    cout << *max_element(all(ans));
 
     quit;
 }
