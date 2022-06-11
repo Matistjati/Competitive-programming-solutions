96c0c98aa34cc4ae8db1669664afa086fbf6caad
Matistjati <matistjati@outlook.com>
2022-06-11 19:42:30 +0200

    Optimized rymdpromenad

--- po/pocamp18/rymdpromenad.cpp
+++ po/pocamp18/rymdpromenad.cpp
@@ -1,43 +1,48 @@
+#undef _GLIBCXX_DEBUG                // disable run-time bound checking, etc
+#pragma GCC optimize("Ofast,inline") // Ofast = O3,fast-math,allow-store-data-races,no-protect-parens
+#pragma GCC optimize ("unroll-loops")
+
+#pragma GCC target("bmi,bmi2,lzcnt,popcnt")                      // bit manipulation
+#pragma GCC target("movbe")                                      // byte swap
+#pragma GCC target("aes,pclmul,rdrnd")                           // encryption
+#pragma GCC target("avx,avx2,f16c,fma,sse3,ssse3,sse4.1,sse4.2") // SIMD
+
 #include <bits/stdc++.h>
 //#include <bits/extc++.h>
 
 using namespace std;
 
-#pragma GCC target ("avx2")
-#pragma GCC optimization ("O3")
-#pragma GCC optimization ("unroll-loops")
-
-#define enablell 1
+#define enablell 0
 
 #define ll long long
 #if enablell
 #define int ll
-#endif
-#define vi vector<int>
-#define vb vector<bool>
-#define vvb vector<vb>
-#define vvvb vector<vvb>
-#define vvi vector<vi>
-#define vvvi vector<vvi>
-#define vvvvi vector<vvvi>
-#define p2 pair<int, int>
-#define p3 tuple<int,int,int>
-#define p4 tuple<int,int,int,int>
-#define vp2 vector<p2>
-#define vvp2 vector<vp2>
-#define vvvp2 vector<vvp2>
-#define vp3 vector<p3>
-#define vvp3 vector<vp3>
-#define vvvp3 vector<vvp3>
-#define vp4 vector<p4>
-#if enablell
-#define inf 2e18
+#define inf LLONG_MAX
+#define float double
 #else
 #define inf int(2e9)
 #endif
+#define vi vector<int>
+#define vvi vector<vi>
+#define vvvi vector<vvi>
+#define vvvvi vector<vvvi>
+#define vb vector<bool>
+#define vvb vector<vb>
+#define vvvb vector<vvb>
+#define p2 pair<int, int>
+#define vp2 vector<p2>
+#define vvp2 vector<vp2>
+#define vvvp2 vector<vvp2>
+#define p3 tuple<int,int,int>
+#define vp3 vector<p3>
+#define vvp3 vector<vp3>
+#define vvvp3 vector<vvp3>
+#define p4 tuple<int,int,int,int>
+#define vp4 vector<p4>
 
 #define read(a) cin >> a
 #define read2(a,b) cin >> a >> b
+#define read3(a,b,c) cin >> a >> b >> c
 #define write(a) cout << (a) << "\n"
 #define quit cout << endl; _Exit(0);
 #define dread(type, a) type a; cin >> a
@@ -48,39 +53,57 @@ using namespace std;
 #ifdef _DEBUG
 #define noop cout << "";
 #define deb __debugbreak();
+#define debassert(expr) if (!(expr)) deb;
+#define debif(expr) if(expr) deb;
 #else
 #define noop ;
 #define deb ;
+#define debassert(expr) ;
+#define debif(expr) ;
 #endif
 
 #define rep(i, high) for (int i = 0; i < high; i++)
 #define repp(i, low, high) for (int i = low; i < high; i++)
 #define repe(i, container) for (auto& i : container)
-#define per(i, high) for (int i = high; i >= 0; i--)
-#define perr(i, low, high) for (int i = high; i >= low; i--)
-#define debassert(expr) if (!(expr)) deb;
+#define per(i, high) for (int i = high-1; i >= 0; i--)
+#define perr(i, low, high) for (int i = high-1; i >= low; i--)
 
-#define readpush(type,vect) type temp; read(temp); vect.(temp);
 #define readvector(type, name, size) vector<type> name(size); rep(i,size) {dread(type,temp); name[i]=temp;}
-#define readinsert(type,a) {type temp; read(temp); a.insert(temp);}
 #define all(a) begin(a),end(a)
 #define setcontains(set, x) (set.find(x) != set.end())
 #define stringcontains(str, x) (str.find(x) != string::npos)
 #define within(a, b, c, d) (a >= 0 && a < b && c >= 0 && c < d)
-#define insideinterval(v, interval) (v >= interval.first && v <= interval.second)
 #define sz(container) ((int)container.size())
 #define mp(a,b) (make_pair(a,b))
 #define first(a) (*begin(a))
+#define indexpair(p, i) ((i==0)?p.first:p.second)
+#define chmax(a,b) ((a)=max((a),b))
+#define chmin(a,b) ((a)=min((a),b))
 
 #define ceildiv(x,y) ((x + y - 1) / y)
 #define fract(a) (a-floor(a))
 
 auto Start = chrono::high_resolution_clock::now();
+#define elapsedmillis() (chrono::duration_cast<chrono::milliseconds>(chrono::high_resolution_clock::now() - Start).count())
+#define rununtil(time) if (elapsedmillis() >= time) break;
 
-inline void fast() {ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);}
+inline void fast() { ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); }
+template <typename T, typename U> inline void operator+=(std::pair<T, U>& l, const std::pair<T, U>& r) { l = { l.first + r.first,l.second + r.second }; }
+template <typename T> inline int sgn(T val) { return (T(0) < val) - (val < T(0)); }
+template <typename Out> inline void split(const string& s, char delim, Out result) { istringstream iss(s); string item; while (getline(iss, item, delim)) { *result++ = item; } }
+inline vector<string> split(const string& s, char delim) { vector<string> elems; split(s, delim, back_inserter(elems)); return elems; }
 
-template <typename T, typename U> void operator+=(std::pair<T, U>& l, const std::pair<T, U>& r) { l = { l.first + r.first,l.second + r.second };}
-template <typename T> int sgn(T val) {return (T(0) < val) - (val < T(0));}
+inline int readint()
+{
+    int v = 0;
+    char c;
+    while ((c = getchar()) != EOF && c != ' ' && c != '\n')
+    {
+        v *= 10;
+        v += c - '0';
+    }
+    return v;
+}
 
 int32_t main()
 {
@@ -88,19 +111,22 @@ int32_t main()
 
 #if 0
     ifstream cin("C:\\Users\\Matis\\source\\repos\\Comp prog\\x64\\Debug\\in.txt");
+    //ifstream cin("C:\\Users\\Matis\\Downloads\\pixel\\examples\\sample01.in");
 #endif
 
-    read2(n, m);
+    int n = readint();
+    int m = readint();
 
-    vi clockWise(m);
+    vi clockwise(max(n, m) + 100);
 
 
     int curr = 1;
-    int v = 0;
-    int rot = 0;
+    ll v = 0;
+    ll rot = 0;
+
     rep(i, m)
     {
-        dread(int, next);
+        int next = readint();
 
         int forwardCost;
 
@@ -115,26 +141,28 @@ int32_t main()
 
 
         v += forwardCost;
-        clockWise[i] = forwardCost;
+        clockwise[forwardCost]++;
 
         curr = next;
     }
 
     rot = v;
 
-    sort(all(clockWise),greater<int>());
-
-
-
-
-    rep(i, m)
+    per(cost, max(n, m) + 100)
     {
-        int old = v + abs(rot);
-        int newV = v + n - 2*clockWise[i] + abs(rot - n);
-        if (newV < old)
+        rep(k, clockwise[cost])
         {
-            v = v + n-2 * clockWise[i];
-            rot = rot - n;
+            ll old = v + abs(rot);
+            ll newV = v + n - 2 * cost + abs(rot - n);
+            if (newV < old)
+            {
+                v = v + n - 2 * cost;
+                rot = rot - n;
+            }
+            else
+            {
+                break;
+            }
         }
     }
 
