964a726f2326277d715867b7e54dd7e60749b890
Matistjati <matistjati@outlook.com>
2022-06-12 21:24:44 +0200

    Optimized gratis mat to get first place

--- po/pofinal16/gratismat.cpp
+++ po/pofinal16/gratismat.cpp
@@ -1,124 +1,151 @@
-#include <iostream>
-#include <vector>
-#include <map>
-#include <sstream>
-#include <algorithm>
-#include <cmath>
-#include <set>
-#include <unordered_set>
-#include <string>
-#include <iterator>
-#include <queue>
-#include <tuple>
-#include <numeric>
-#include <random>
-#include <time.h>
-#include <stack>
-#include <chrono>
-#include <unordered_map>
+#undef _GLIBCXX_DEBUG                // disable run-time bound checking, etc
+#pragma GCC optimize("Ofast,inline") // Ofast = O3,fast-math,allow-store-data-races,no-protect-parens
+#pragma GCC optimize ("unroll-loops")
+
+#pragma GCC target("bmi,bmi2,lzcnt,popcnt")                      // bit manipulation
+#pragma GCC target("movbe")                                      // byte swap
+#pragma GCC target("aes,pclmul,rdrnd")                           // encryption
+#pragma GCC target("avx,avx2,f16c,fma,sse3,ssse3,sse4.1,sse4.2") // SIMD
+
+#include <bits/stdc++.h>
+//#include <bits/extc++.h>
 
 using namespace std;
 
+#define enablell 0
+
 #define ll long long
-#define vi vector<ll>
+#if enablell
+#define int ll
+#define inf LLONG_MAX
+#define float double
+#else
+#define inf int(2e9)
+#endif
+#define vi vector<int>
 #define vvi vector<vi>
-#define p2 pair<ll, ll>
-#define p3 vi
-#define p4 vi
-#define inf 2e9
+#define vvvi vector<vvi>
+#define vvvvi vector<vvvi>
+#define vb vector<bool>
+#define vvb vector<vb>
+#define vvvb vector<vvb>
+#define p2 pair<int, int>
+#define vp2 vector<p2>
+#define vvp2 vector<vp2>
+#define vvvp2 vector<vvp2>
+#define p3 tuple<int,int,int>
+#define vp3 vector<p3>
+#define vvp3 vector<vp3>
+#define vvvp3 vector<vvp3>
+#define p4 tuple<int,int,int,int>
+#define vp4 vector<p4>
 
 #define read(a) cin >> a
-#define write(a) cout << (a) << endl
+#define read2(a,b) cin >> a >> b
+#define read3(a,b,c) cin >> a >> b >> c
+#define write(a) cout << (a) << "\n"
+#define quit cout << endl; _Exit(0);
+#define dread(type, a) type a; cin >> a
+#define dread2(type, a, b) dread(type, a); dread(type, b)
+#define dread3(type, a, b, c) dread2(type, a, b); dread(type, c)
+#define dread4(type, a, b, c, d) dread3(type, a, b, c); dread(type, d)
+#define dread5(type, a, b, c, d, e) dread4(type, a, b, c, d); dread(type, e)
+#ifdef _DEBUG
+#define noop cout << "";
 #define deb __debugbreak();
+#define debif(expr) if(expr) deb;
+#else
+#define noop ;
+#define deb ;
+#define debif(expr) ;
+#endif
 
-#define readpush(type,a) type temp; read(temp); a.push_back(temp)
-#define readinsert(type,a) type temp; read(temp); a.insert(temp)
-#define setcontains(set, x) (set.find(x) != set.end())
-#define all(a) begin(a),end(a)
-
-#define rep(i, high) for (ll i = 0; i < high; i++)
+#define rep(i, high) for (int i = 0; i < high; i++)
+#define repp(i, low, high) for (int i = low; i < high; i++)
 #define repe(i, container) for (auto& i : container)
-#define per(i, high) for (ll i = high; i >= 0; i--)
+#define per(i, high) for (int i = high-1; i >= 0; i--)
+#define perr(i, low, high) for (int i = high-1; i >= low; i--)
+
+#define readvector(type, name, size) vector<type> name(size); rep(i,size) {dread(type,temp); name[i]=temp;}
+#define all(a) begin(a),end(a)
+#define setcontains(set, x) (set.find(x) != set.end())
+#define stringcontains(str, x) (str.find(x) != string::npos)
+#define within(a, b, c, d) (a >= 0 && a < b && c >= 0 && c < d)
+#define sz(container) ((int)container.size())
+#define mp(a,b) (make_pair(a,b))
+#define first(a) (*begin(a))
+#define indexpair(p, i) ((i==0)?p.first:p.second)
+#define chmax(a,b) ((a)=max((a),b))
+#define chmin(a,b) ((a)=min((a),b))
 
 #define ceildiv(x,y) ((x + y - 1) / y)
+#define fract(a) (a-floor(a))
 
+auto Start = chrono::high_resolution_clock::now();
+#define elapsedmillis() (chrono::duration_cast<chrono::milliseconds>(chrono::high_resolution_clock::now() - Start).count())
+#define rununtil(time) if (elapsedmillis() >= time) break;
 
-inline void fast()
+inline void fast() { ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); }
+template <typename T, typename U> inline void operator+=(std::pair<T, U>& l, const std::pair<T, U>& r) { l = { l.first + r.first,l.second + r.second }; }
+template <typename T> inline int sgn(T val) { return (T(0) < val) - (val < T(0)); }
+template <typename Out> inline void split(const string& s, char delim, Out result) { istringstream iss(s); string item; while (getline(iss, item, delim)) { *result++ = item; } }
+inline vector<string> split(const string& s, char delim) { vector<string> elems; split(s, delim, back_inserter(elems)); return elems; }
+inline int readint() { int v = 0; char c; while ((c = getchar()) != EOF && c != ' ' && c != '\n') { v *= 10; v += c - '0'; } return v; }
+
+// Optimizations to get #1:
+// fast int input
+// bitset instead of bool array
+// only having downwards connections, thus removing need to keep track of parent
+
+#define bset bitset<100010>
+
+int n_can_buy(vector<vector<int>>& edges, bset& table, int node)
 {
-    ios::sync_with_stdio(false);
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
-}
-
-int n_can_buy(map<int, set<int>>& edges, vector<bool>& table, vector<bool>& visited, int node)
-{
-    if (table[node])
-    {
-        return 1;
-    }
-    if (visited[node])
-    {
-        return 0;
-    }
-    else
-    {
-        visited[node] = true;
-    }
+    if (table[node]) return 1;
 
     int ret = 0;
 
-    repe(edge,edges[node])
-    {
-        ret += n_can_buy(edges, table, visited, edge);
-    }
+    repe(edge, edges[node]) ret += n_can_buy(edges, table, edge);
 
     return ret;
 }
 
-int main()
+int32_t main()
 {
     fast();
 
-    int n;
-    int m;
-    read(n);
-    read(m);
+#if 0
+    ifstream cin("C:\\Users\\Matis\\source\\repos\\Comp prog\\x64\\Debug\\in.txt");
+#endif
 
-    map<int, set<int>> connections;
-    vector<bool> employees(n+1);
+    int n = readint();
+    int m = readint();
+
+    vvi connections(n);
+    bset employees;
     int start = -1;
     rep(i, n)
     {
-        int temp;
-        read(temp);
-        temp--;
-        if (temp == -1)
+        int boss = readint();
+        boss--;
+        if (boss == -1)
         {
             start = i;
             continue;
         }
-        if (!setcontains(connections,temp))
-        {
-            connections[temp] = {};
-        }
-        if (!setcontains(connections,i))
-        {
-            connections[i] = {};
-        }
 
-        connections[temp].insert(i);
-        connections[i].insert(temp);
+        connections[boss].emplace_back(i);
     }
+
     rep(i, m)
     {
-        int temp;
-        read(temp);
+        int temp = readint();
         temp--;
-        employees[temp] = 1;
+        employees[temp] = true;
     }
 
-    vector<bool> visited(n+1);
-    write(n_can_buy(connections, employees, visited, start));
 
-    return 0;
+    write(n_can_buy(connections, employees, start));
+
+    quit;
 }
