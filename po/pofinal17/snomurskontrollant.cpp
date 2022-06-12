2cd33a6c048a75fcf308b1804f3b0c86bd9c7755
Matistjati <matistjati@outlook.com>
2022-06-12 21:35:55 +0200

    Further optimized snömurskontrollanten

--- po/pofinal17/snomurskontrollant.cpp
+++ po/pofinal17/snomurskontrollant.cpp
@@ -1,61 +1,97 @@
+#undef _GLIBCXX_DEBUG                // disable run-time bound checking, etc
+#pragma GCC optimize("Ofast,inline") // Ofast = O3,fast-math,allow-store-data-races,no-protect-parens
+#pragma GCC optimize ("unroll-loops")
+
+#pragma GCC target("bmi,bmi2,lzcnt,popcnt")                      // bit manipulation
+#pragma GCC target("movbe")                                      // byte swap
+#pragma GCC target("aes,pclmul,rdrnd")                           // encryption
+#pragma GCC target("avx,avx2,f16c,fma,sse3,ssse3,sse4.1,sse4.2") // SIMD
+
 #include <bits/stdc++.h>
+//#include <bits/extc++.h>
 
 using namespace std;
 
-#pragma GCC target ("avx2")
-#pragma GCC optimization ("O3")
-#pragma GCC optimization ("unroll-loops")
+#define enablell 0
 
 #define ll long long
-#define vi vector<ll>
+#if enablell
+#define int ll
+#define inf LLONG_MAX
+#define float double
+#else
+#define inf int(2e9)
+#endif
+#define vi vector<int>
 #define vvi vector<vi>
-#define p2 pair<ll, ll>
-#define p3 vi
-#define p4 vi
-#define ip3 tuple<int,int,int>
-#define ip4 tuple<int,int,int,int>
+#define vvvi vector<vvi>
+#define vvvvi vector<vvvi>
+#define vb vector<bool>
+#define vvb vector<vb>
+#define vvvb vector<vvb>
+#define p2 pair<int, int>
 #define vp2 vector<p2>
+#define vvp2 vector<vp2>
+#define vvvp2 vector<vvp2>
+#define p3 tuple<int,int,int>
 #define vp3 vector<p3>
-#define inf 2e9
-#define linf 1e17
+#define vvp3 vector<vp3>
+#define vvvp3 vector<vvp3>
+#define p4 tuple<int,int,int,int>
+#define vp4 vector<p4>
 
 #define read(a) cin >> a
+#define read2(a,b) cin >> a >> b
+#define read3(a,b,c) cin >> a >> b >> c
 #define write(a) cout << (a) << "\n"
+#define quit cout << endl; _Exit(0);
 #define dread(type, a) type a; cin >> a
 #define dread2(type, a, b) dread(type, a); dread(type, b)
 #define dread3(type, a, b, c) dread2(type, a, b); dread(type, c)
 #define dread4(type, a, b, c, d) dread3(type, a, b, c); dread(type, d)
 #define dread5(type, a, b, c, d, e) dread4(type, a, b, c, d); dread(type, e)
 #ifdef _DEBUG
+#define noop cout << "";
 #define deb __debugbreak();
+#define debif(expr) if(expr) deb;
 #else
+#define noop ;
 #define deb ;
+#define debif(expr) ;
 #endif
 
-#define rep(i, high) for (ll i = 0; i < high; i++)
-#define repp(i, low, high) for (ll i = low; i < high; i++)
+#define rep(i, high) for (int i = 0; i < high; i++)
+#define repp(i, low, high) for (int i = low; i < high; i++)
 #define repe(i, container) for (auto& i : container)
-#define per(i, high) for (ll i = high; i >= 0; i--)
+#define per(i, high) for (int i = high-1; i >= 0; i--)
+#define perr(i, low, high) for (int i = high-1; i >= low; i--)
 
-#define readpush(type,vect) type temp; read(temp); vect.push_back(temp);
 #define readvector(type, name, size) vector<type> name(size); rep(i,size) {dread(type,temp); name[i]=temp;}
-#define readinsert(type,a) {type temp; read(temp); a.insert(temp);}
 #define all(a) begin(a),end(a)
 #define setcontains(set, x) (set.find(x) != set.end())
 #define stringcontains(str, x) (str.find(x) != string::npos)
 #define within(a, b, c, d) (a >= 0 && a < b && c >= 0 && c < d)
+#define sz(container) ((int)container.size())
+#define mp(a,b) (make_pair(a,b))
+#define first(a) (*begin(a))
+#define indexpair(p, i) ((i==0)?p.first:p.second)
+#define chmax(a,b) ((a)=max((a),b))
+#define chmin(a,b) ((a)=min((a),b))
 
 #define ceildiv(x,y) ((x + y - 1) / y)
 #define fract(a) (a-floor(a))
-#define sign(a) ((a>0) ? 1 : -1)
 
 auto Start = chrono::high_resolution_clock::now();
+#define elapsedmillis() (chrono::duration_cast<chrono::milliseconds>(chrono::high_resolution_clock::now() - Start).count())
+#define rununtil(time) if (elapsedmillis() >= time) break;
 
-inline void fast()
-{
-    ios::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
-}
+inline void fast() { ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); }
+template <typename T, typename U> inline void operator+=(std::pair<T, U>& l, const std::pair<T, U>& r) { l = { l.first + r.first,l.second + r.second }; }
+template <typename T> inline int sgn(T val) { return (T(0) < val) - (val < T(0)); }
+template <typename Out> inline void split(const string& s, char delim, Out result) { istringstream iss(s); string item; while (getline(iss, item, delim)) { *result++ = item; } }
+inline vector<string> split(const string& s, char delim) { vector<string> elems; split(s, delim, back_inserter(elems)); return elems; }
+inline int readint() { int v = 0; char c; while ((c = getchar()) != EOF && c != ' ' && c != '\n') { v *= 10; v += c - '0'; } return v; }
+
 
 void die()
 {
@@ -70,7 +106,7 @@ inline void endpointcheck(vp2& row, int w)
         die();
     }
 
-    if (row[row.size()-1].first + row[row.size()-1].second != w)
+    if (row[row.size() - 1].first + row[row.size() - 1].second != w)
     {
         die();
     }
@@ -78,7 +114,7 @@ inline void endpointcheck(vp2& row, int w)
 
 inline void holecheck(vp2& row, vp2& prevRow)
 {
-    if (prevRow.size()==0)
+    if (prevRow.size() == 0)
     {
         return;
     }
@@ -92,9 +128,9 @@ inline void holecheck(vp2& row, vp2& prevRow)
             while (true)
             {
                 p2 p = prevRow[j];
-                if (p.first+p.second > prev)
+                if (p.first + p.second > prev)
                 {
-                    if (p.second+p.first>=curr.first)
+                    if (p.second + p.first >= curr.first)
                     {
                         break;
                     }
@@ -130,7 +166,7 @@ inline void edgecheck(vp2& row, vp2& prevRow, int w)
             while (true)
             {
                 p2 p = prevRow[j];
-                if (p.first + p.second > curr.first-1)
+                if (p.first + p.second > curr.first - 1)
                 {
                     if (p.first <= curr.first)
                     {
@@ -178,7 +214,7 @@ inline void edgecheck(vp2& row, vp2& prevRow, int w)
     }
 }
 
-int main()
+int32_t main()
 {
     fast();
 
@@ -186,16 +222,18 @@ int main()
     ifstream cin("C:\\Users\\Matis\\source\\repos\\Comp prog\\x64\\Debug\\in.txt");
 #endif
 
-    dread2(int, w, h);
+    int w = readint();
+    int h = readint();
 
     vector<p2> prevRow;
     rep(i, h)
     {
-        dread(int, n);
+        int n = readint();
         vector<p2> row(n);
         rep(i, n)
         {
-            dread2(int, start, length);
+            int start = readint();
+            int length = readint();
             row[i] = { start,length };
         }
 
@@ -208,6 +246,5 @@ int main()
 
     write("YES");
 
-    cout << endl;
-    _Exit(0);
+    quit;
 }
