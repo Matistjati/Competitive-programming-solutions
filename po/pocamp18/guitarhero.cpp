7a7bd423ffa10ba9bb38e701ab76890a336bcb4a
Matistjati <matistjati@outlook.com>
2022-06-15 23:55:41 +0200

    Optimized guitar hero

--- po/pocamp18/guitarhero.cpp
+++ po/pocamp18/guitarhero.cpp
@@ -1,40 +1,48 @@
+#undef _GLIBCXX_DEBUG                // disable run-time bound checking, etc
+#pragma GCC optimize("Ofast,inline") // Ofast = O3,fast-math,allow-store-data-races,no-protect-parens
+#pragma GCC optimize ("unroll-loops")
+
+#pragma GCC target("bmi,bmi2,lzcnt,popcnt")                      // bit manipulation
+#pragma GCC target("movbe")                                      // byte swap
+#pragma GCC target("aes,pclmul,rdrnd")                           // encryption
+#pragma GCC target("avx,avx2,f16c,fma,sse3,ssse3,sse4.1,sse4.2") // SIMD
+
 #include <bits/stdc++.h>
 //#include <bits/extc++.h>
 
 using namespace std;
-
-#pragma GCC target ("avx2")
-#pragma GCC optimization ("O3")
-#pragma GCC optimization ("unroll-loops")
 
 #define enablell 0
 
 #define ll long long
 #if enablell
 #define int ll
+#define inf LLONG_MAX
+#define float double
+#else
+#define inf int(2e9)
 #endif
 #define vi vector<int>
-#define vb vector<bool>
-#define vvb vector<vb>
-#define vvvb vector<vvb>
 #define vvi vector<vi>
 #define vvvi vector<vvi>
 #define vvvvi vector<vvvi>
+#define vb vector<bool>
+#define vvb vector<vb>
+#define vvvb vector<vvb>
 #define p2 pair<int, int>
-#define p3 tuple<int,int,int>
-#define p4 tuple<int,int,int,int>
 #define vp2 vector<p2>
 #define vvp2 vector<vp2>
+#define vvvp2 vector<vvp2>
+#define p3 tuple<int,int,int>
 #define vp3 vector<p3>
+#define vvp3 vector<vp3>
+#define vvvp3 vector<vvp3>
+#define p4 tuple<int,int,int,int>
 #define vp4 vector<p4>
-#if enablell
-#define inf 1e18
-#else
-#define inf int(1e9)
-#endif
 
 #define read(a) cin >> a
 #define read2(a,b) cin >> a >> b
+#define read3(a,b,c) cin >> a >> b >> c
 #define write(a) cout << (a) << "\n"
 #define quit cout << endl; _Exit(0);
 #define dread(type, a) type a; cin >> a
@@ -45,67 +53,68 @@ using namespace std;
 #ifdef _DEBUG
 #define noop cout << "";
 #define deb __debugbreak();
+#define debif(expr) if(expr) deb;
+#define debassert(expr) if(!(expr)) deb;
 #else
 #define noop ;
 #define deb ;
+#define debif(expr) ;
+#define debassert(expr) ;
 #endif
 
 #define rep(i, high) for (int i = 0; i < high; i++)
 #define repp(i, low, high) for (int i = low; i < high; i++)
 #define repe(i, container) for (auto& i : container)
-#define per(i, high) for (int i = high; i >= 0; i--)
-#define perr(i, low, high) for (int i = high; i >= low; i--)
+#define per(i, high) for (int i = high-1; i >= 0; i--)
+#define perr(i, low, high) for (int i = high-1; i >= low; i--)
 
-#define readpush(type,vect) type temp; read(temp); vect.(temp);
 #define readvector(type, name, size) vector<type> name(size); rep(i,size) {dread(type,temp); name[i]=temp;}
-#define readinsert(type,a) {type temp; read(temp); a.insert(temp);}
 #define all(a) begin(a),end(a)
 #define setcontains(set, x) (set.find(x) != set.end())
 #define stringcontains(str, x) (str.find(x) != string::npos)
 #define within(a, b, c, d) (a >= 0 && a < b && c >= 0 && c < d)
-#define insideinterval(v, interval) (v >= interval.first && v <= interval.second)
 #define sz(container) ((int)container.size())
 #define mp(a,b) (make_pair(a,b))
 #define first(a) (*begin(a))
+#define indexpair(p, i) ((i==0)?p.first:p.second)
+#define chmax(a,b) ((a)=max((a),b))
+#define chmin(a,b) ((a)=min((a),b))
 
 #define ceildiv(x,y) ((x + y - 1) / y)
 #define fract(a) (a-floor(a))
-#define sign(a) ((a>0) ? 1 : -1)
 
 auto Start = chrono::high_resolution_clock::now();
+#define elapsedmillis() (chrono::duration_cast<chrono::milliseconds>(chrono::high_resolution_clock::now() - Start).count())
+#define rununtil(time) if (elapsedmillis() >= time) break;
 
-inline void fast()
-{
-    ios::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
-}
-
-template <typename T, typename U>
-void operator+=(std::pair<T, U>& l, const std::pair<T, U>& r)
-{
-    l = { l.first + r.first,l.second + r.second };
-}
+inline void fast() { ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); }
+template <typename T, typename U> inline void operator+=(std::pair<T, U>& l, const std::pair<T, U>& r) { l = { l.first + r.first,l.second + r.second }; }
+template <typename T> inline int sgn(T val) { return (T(0) < val) - (val < T(0)); }
+template <typename Out> inline void split(const string& s, char delim, Out result) { istringstream iss(s); string item; while (getline(iss, item, delim)) { *result++ = item; } }
+inline vector<string> split(const string& s, char delim) { vector<string> elems; split(s, delim, back_inserter(elems)); return elems; }
+inline int readintsigned() { int v = 0; int sign = 1; char c = getchar(); if (c == '-') { sign = -1; } else { v += c - '0'; } while ((c = getchar()) != EOF && c != ' ' && c != '\n') { v *= 10; v += c - '0'; } return v * sign; }
+inline int readint() { int v = 0; char c; while ((c = getchar()) != EOF && c != ' ' && c != '\n') { v *= 10; v += c - '0'; } return v; }
+inline string readstring() { string s; char c; while ((c = getchar()) != EOF && c != '\n') { s.push_back(c); } return s; }
 
 #define NONE 0
 #define INCREASING 1
 #define DECREASING 2
-
-
-
 
 int32_t main()
 {
     fast();
 
 #if 0
-    ifstream cin("C:\\Users\\Matis\\source\\repos\\Comp prog\\x64\\Release\\in.txt");
+    //ifstream cin("C:\\Users\\Matis\\source\\repos\\Comp prog\\x64\\Debug\\in.txt");
+    ifstream cin("C:\\Users\\Matis\\desktop\\kryssring\\attachments\\5.in");
 #endif
 
-    dread3(int, n, m, q);
+    int n, m, q;
+    n = readint(); m = readint(); q = readint();
 
-    readvector(int, notes, n);
+    vi notes(n);
+    rep(i, n) notes[i] = readint();
 
-    vi illegalIntervals;
 
     int prev = notes[0];
     int prevBegin = 0;
@@ -113,7 +122,7 @@ int32_t main()
 
 
     vp2 intervalsToProcess;
-    vi indexMap(n);
+    vi compress(n);
     int index = 0;
 
     repp(i, 1, notes.size())
@@ -148,7 +157,7 @@ int32_t main()
             state = DECREASING;
             index++;
         }
-        indexMap[i] = index;
+        compress[i] = index;
     }
 
     if (index - prevBegin >= m)
@@ -156,37 +165,63 @@ int32_t main()
         intervalsToProcess.emplace_back(prevBegin, index);
     }
 
+    vi illegalIntervals;
+
     repe(interval, intervalsToProcess)
     {
-        for (int i = interval.first; i+m <= interval.second; i++)
+        for (int i = interval.first; i + m <= interval.second; i++)
         {
             illegalIntervals.emplace_back(i);
         }
     }
-    sort(all(illegalIntervals));
+
+    index++;
+    vi nextinterval(index, -inf);
+
+    int p = 0;
+
+    rep(i, index)
+    {
+        if (illegalIntervals.empty()) break;
+        while (illegalIntervals[p] < i)
+        {
+            p++;
+            if (p == illegalIntervals.size()) goto done;
+        }
+
+        nextinterval[i] = p;
+    }
+
+done:;
 
     while (q--)
     {
-        bool works = true;
-        dread2(int, left, right);
-        left = indexMap[left-1];
-        right = indexMap[right-1];
+        int left = readint();
+        int right = readint();
 
-        auto it = lower_bound(all(illegalIntervals), left);
+        left = compress[left - 1];
+        right = compress[right - 1];
 
-        if (it==illegalIntervals.end())
+        int idx = nextinterval[left];
+        // There are no illegal intervals to the right of left
+        if (idx == -inf)
         {
             cout << "ja\n";
             continue;
         }
 
-        if (*it+m <= right)
+        int firstIllegal = illegalIntervals[idx];
+        // The illegal interval lies outside of right
+        if (firstIllegal + m > right)
+        {
+            cout << "ja\n";
+        }
+        else
         {
             cout << "nej\n";
-            continue;
         }
-        cout << "ja\n";
     }
+
 
 
     quit;
