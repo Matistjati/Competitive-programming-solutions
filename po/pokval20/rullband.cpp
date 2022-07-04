bc56c035c71cb828aa87211cd38810f8a6ece871
Matistjati <matistjati@outlook.com>
2022-07-04 22:27:26 +0200

    Improved rullband

--- po/pokval20/rullband.cpp
+++ po/pokval20/rullband.cpp
@@ -1,131 +1,182 @@
-#include <iostream>
-#include <vector>
-#include <map>
-#include <sstream>
-#include <algorithm>
-#include <cmath>
-#include <set>
-#include <unordered_set>
-#include <string>
-#include <iterator>
-#include <queue>
-#include <tuple>
-#include <numeric>
-#include <random>
-#include <time.h>
+#undef _GLIBCXX_DEBUG                // disable run-time bound checking, etc
+#pragma GCC optimize("Ofast,inline") // Ofast = O3,fast-math,allow-store-data-races,no-protect-parens
+#pragma GCC optimize ("unroll-loops")
 
+#pragma GCC target("bmi,bmi2,lzcnt,popcnt")                      // bit manipulation
+#pragma GCC target("movbe")                                      // byte swap
+#pragma GCC target("aes,pclmul,rdrnd")                           // encryption
+#pragma GCC target("avx,avx2,f16c,fma,sse3,ssse3,sse4.1,sse4.2") // SIMD
+
+#include <bits/stdc++.h>
+//#include <bits/extc++.h>
 
 using namespace std;
 
+#define enablell 0
+
+typedef long long ll;
+#if enablell
+#define int ll
+#define inf LLONG_MAX
+#define float double
+#else
+#define inf int(2e9)
+#endif
+#define vi vector<int>
+#define vvi vector<vi>
+#define vvvi vector<vvi>
+#define vvvvi vector<vvvi>
+#define vb vector<bool>
+#define vvb vector<vb>
+#define vvvb vector<vvb>
 #define p2 pair<int, int>
-#define ll long long
-#define inf 1e9
+#define vp2 vector<p2>
+#define vvp2 vector<vp2>
+#define vvvp2 vector<vvp2>
+#define p3 tuple<int,int,int>
+#define vp3 vector<p3>
+#define vvp3 vector<vp3>
+#define vvvp3 vector<vvp3>
+#define p4 tuple<int,int,int,int>
+#define vp4 vector<p4>
 
 #define read(a) cin >> a
-#define write(a) cout << (a) << endl
+#define read2(a,b) cin >> a >> b
+#define read3(a,b,c) cin >> a >> b >> c
+#define write(a) cout << (a) << "\n"
+#define quit cout << endl; _Exit(0);
+#define dread(type, a) type a; cin >> a
+#define dread2(type, a, b) dread(type, a); dread(type, b)
+#define dread3(type, a, b, c) dread2(type, a, b); dread(type, c)
+#define dread4(type, a, b, c, d) dread3(type, a, b, c); dread(type, d)
+#define dread5(type, a, b, c, d, e) dread4(type, a, b, c, d); dread(type, e)
+#ifdef _DEBUG
+#define noop cout << "";
+#define deb __debugbreak();
+#define debif(expr) if(expr) deb;
+#define debassert(expr) if(!(expr)) deb;
+#else
+#define noop ;
+#define deb ;
+#define debif(expr) ;
+#define debassert(expr) ;
+#endif
 
-#define readpush(type,a) type temp; read(temp); a.push_back(temp)
-#define readinsert(type,a) type temp; read(temp); a.insert(temp)
-#define setcontains(set, x) (set.find(x) != set.end())
-
-#define rep(i, high) for (ll i = 0; i < high; i++)
+#define rep(i, high) for (int i = 0; i < high; i++)
+#define repp(i, low, high) for (int i = low; i < high; i++)
 #define repe(i, container) for (auto& i : container)
-#define per(i, high) for (ll i = high; i >= 0; i--)
+#define per(i, high) for (int i = high-1; i >= 0; i--)
+#define perr(i, low, high) for (int i = high-1; i >= low; i--)
+
+#define readvector(type, name, size) vector<type> name(size); rep(i,size) {dread(type,temp); name[i]=temp;}
+#define all(a) begin(a),end(a)
+#define setcontains(set, x) (set.find(x) != set.end())
+#define stringcontains(str, x) (str.find(x) != string::npos)
+#define within(a, b, c, d) (a >= 0 && a < b && c >= 0 && c < d)
+#define sz(container) ((int)container.size())
+#define mp(a,b) (make_pair(a,b))
+#define first(a) (*begin(a))
+#define indexpair(p, i) ((i==0)?p.first:p.second)
+#define chmax(a,b) ((a)=max((a),b))
+#define chmin(a,b) ((a)=min((a),b))
 
 #define ceildiv(x,y) ((x + y - 1) / y)
+#define fract(a) (a-floor(a))
+
+auto Start = chrono::high_resolution_clock::now();
+#define elapsedmillis() (chrono::duration_cast<chrono::milliseconds>(chrono::high_resolution_clock::now() - Start).count())
+#define rununtil(time) if (elapsedmillis() >= time) break;
+
+inline void fast() { ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); }
+template <typename T, typename U> inline void operator+=(std::pair<T, U>& l, const std::pair<T, U>& r) { l = { l.first + r.first,l.second + r.second }; }
+inline p2 operator+(const p2& l, const p2& r) { return mp(l.first + r.first, l.second + r.second); }
+template <typename T> inline int sgn(T val) { return (T(0) < val) - (val < T(0)); }
+template <typename Out> inline void split(const string& s, char delim, Out result) { istringstream iss(s); string item; while (getline(iss, item, delim)) { *result++ = item; } }
+inline vector<string> split(const string& s, char delim) { vector<string> elems; split(s, delim, back_inserter(elems)); return elems; }
+inline int readintsigned() { int v = 0; int sign = 1; char c = getchar(); if (c == '-') { sign = -1; } else { v += c - '0'; } while ((c = getchar()) != EOF && c != ' ' && c != '\n') { v *= 10; v += c - '0'; } return v * sign; }
+inline int readint() { int v = 0; char c; while ((c = getchar()) != EOF && c != ' ' && c != '\n') { v *= 10; v += c - '0'; } return v; }
+inline string readstring() { string s; char c; while ((c = getchar()) != EOF && c != '\n') { s.push_back(c); } return s; }
+#if 1
+random_device rd;
+mt19937 rng(rd());
+#endif
+
+#if _MSC_VER > 0
+#define gc() getchar()
+#else
+#if 0
+#include <bits/extc++.h>
+using namespace __gnu_pbds;
+#endif
+#define gc() getchar_unlocked()
+struct chash { // large odd number for C
+    const uint64_t C = ll(4e18 * acos(0)) | 71;
+    ll operator()(ll x) const { return x; }
+};
+//typedef __gnu_pbds::gp_hash_table<int, null_type, chash> h;
+
+#endif
 
 
-inline void fast()
-{
-    ios::sync_with_stdio(false);
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
-}
-
-int main()
+int32_t main()
 {
     fast();
 
-    int n;
-    int m;
-    int speed;
-    read(n);
-    read(m);
-    read(speed);
+#if 0
+    ifstream cin("C:\\Users\\joshua.jeffmarander\\source\\repos\\Comp prog\\x64\\Debug\\in.txt");
+#endif
 
-    vector<int> fastest(m+1, inf);
-    map<int, set<p2>> belts;
-    map<int, set<p2>> beltEnds;
+    dread3(int, n, m, g);
+
+    vvp2 edges(m + 1);
+
+
 
     rep(i, n)
     {
-        int start;
-        int end;
-        int duration;
-        read(start);
-        read(end);
-        read(duration);
-
-        if (!setcontains(belts,start))
-        {
-            belts[start] = set<p2>();
-        }
-        if (!setcontains(beltEnds, end))
-        {
-            beltEnds[end] = set<p2>();
-        }
-
-        belts[start].insert({ end,duration });
-        beltEnds[end].insert({ start,duration });
+        dread3(int, s, e, l);
+        edges[s].emplace_back(e, l);
     }
 
-    fastest[0] = 0;
-    for (int i = 1; i < m+1; i++)
+    rep(i, m + 1)
     {
-        if (fastest[i-1]+speed < fastest[i])
-        {
-            fastest[i] = fastest[i - 1] + speed;
-        }
-
-        int curr = fastest[i];
-        if (setcontains(belts, i))
-        {
-
-
-            repe(belt, belts[i])
-            {
-                if (curr + belt.second < fastest[belt.first])
-                {
-                    fastest[belt.first] = curr + belt.second;
-                }
-            }
-        }
-
-        int j = i - 1;
-        curr = fastest[i];
-        while ((i-j)*speed+curr<fastest[j])
-        {
-            if (setcontains(belts, j))
-            {
-                repe(belt, belts[j])
-                {
-                    if ((i-j)*speed+curr+belt.second<fastest[belt.first])
-                    {
-                        fastest[belt.first] = (i - j) * speed + curr + belt.second;
-                    }
-                }
-            }
-
-            j--;
-            if (j < 0)
-            {
-                break;
-            }
-        }
-
+        if (i != 0) edges[i].emplace_back(i - 1, g);
+        if (i != m) edges[i].emplace_back(i + 1, g);
     }
 
-    write(fastest[fastest.size() - 1]);
+    vi dist(m + 1, inf);
+    deque<int> pq;
+    pq.push_back(0);
+    dist[0] = 0;
+    bitset<int(2e5) + 1> inq;
 
-    return 0;
+
+    while (pq.size())
+    {
+        int pos = pq.front();
+        pq.pop_front();
+        inq[pos] = false;
+
+
+
+        repe(edge, edges[pos])
+        {
+            int d = dist[pos] + edge.second;
+            if (d<dist[edge.first])
+            {
+                dist[edge.first] = d;
+                if (!inq[edge.first])
+                {
+                    inq[edge.first] = true;
+                    if (pq.size() && dist[pq.front()] < dist[edge.first]) pq.push_back(edge.first);
+                    else pq.push_front(edge.first);
+
+                }
+            }
+        }
+    }
+
+    cout << dist[m];
+
+    quit;
 }
