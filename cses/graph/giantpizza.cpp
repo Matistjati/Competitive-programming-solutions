1b08be43e0818af70b2501ebbbcece484af88489
Matistjati <matistjati@outlook.com>
2023-09-05 19:34:39 +0200

    Solved some cses problems

---
+++ cses/graph/giantpizza.cpp
@@ -0,0 +1,242 @@
+#undef _GLIBCXX_DEBUG                // disable run-time bound checking, etc
+#pragma GCC optimize("Ofast,inline") // Ofast = O3,fast-math,allow-store-data-races,no-protect-parens
+#pragma GCC optimize ("unroll-loops")
+
+#pragma GCC target("bmi,bmi2,lzcnt,popcnt")                      // bit manipulation
+#pragma GCC target("movbe")                                      // byte swap
+#pragma GCC target("aes,pclmul,rdrnd")                           // encryption
+#pragma GCC target("avx,avx2,f16c,fma,sse3,ssse3,sse4.1,sse4.2") // SIMD
+
+#include <bits/stdc++.h>
+#include <bits/extc++.h>
+using namespace std;
+
+#define enablell 1
+
+typedef long long ll;
+typedef unsigned long long ull;
+#if enablell
+#define int ll
+#define inf int(1e18)
+#else
+const int inf = int(2e9);
+#endif
+typedef vector<ull> vull;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<vvi> vvvi;
+typedef vector<vvvi> vvvvi;
+typedef vector<bool> vb;
+typedef vector<vb> vvb;
+typedef vector<vvb> vvvb;
+typedef pair<int, int> p2;
+typedef vector<p2> vp2;
+typedef vector<vp2> vvp2;
+typedef vector<vvp2> vvvp2;
+typedef tuple<int, int, int> p3;
+typedef vector<p3> vp3;
+typedef vector<vp3> vvp3;
+typedef vector<vvp3> vvvp3;
+typedef tuple<int, int, int, int> p4;
+typedef vector<p4> vp4;
+
+#define _LOCAL _MSC_VER > 0
+#if _LOCAL
+
+#define assert(x) debassert(x)
+#define popcount(x) __popcnt(x)
+uint32_t clz(uint32_t x) { return _lzcnt_u32(x); }
+uint32_t ctz(uint32_t x) { return _tzcnt_u32(x); }
+#define bswap64(x) _byteswap_uint64(x)
+#else
+
+#define popcount(x) __builtin_popcountll(x)
+uint32_t clz(uint32_t x) { return __builtin_clz(x); }
+uint32_t ctz(uint32_t x) { return __builtin_ctzll(x); }
+#define bswap64(x) __builtin_bswap64(x)
+
+#if 0
+namespace pbds
+{
+	using namespace __gnu_pbds;
+
+	template<typename T> using indexed_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+	template<typename T, typename U> using indexed_map = tree<T, U, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+	struct chash { // large odd number for C
+		const uint64_t C = ll(4e18 * acos(0)) | 71;
+		ll operator()(ll x) const { return __builtin_bswap64(x * C); }
+	};
+	template<typename T, typename U> using fast_map = __gnu_pbds::gp_hash_table<T, U, chash>;
+	template<typename T> using fast_set = __gnu_pbds::gp_hash_table<T, null_type, chash>;
+	template<typename T, typename H> using fast_set_h = __gnu_pbds::gp_hash_table<T, null_type, H>;
+}
+using namespace pbds;
+#endif
+#endif
+
+inline void fast() { ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); }
+#define FAST_INPUT 0
+#if FAST_INPUT && !_LOCAL
+#define gc() getchar_unlocked()
+inline void read(int& v) { v = 0; int sign = 1; char c = gc(); if (c == '-') { sign = -1; } else { v += c - '0'; } while ((c = gc()) && c != ' ' && c != '\n') { if (c == EOF) { v = -1; return; } v *= 10; v += c - '0'; } v *= sign; }
+inline void read(int& u, int& v) { read(u); read(v); }
+inline void read(int& u, int& v, int& k) { read(u); read(v); read(k); }
+//inline void read(int& v) { char c; while ((c = getchar()) != EOF && c != ' ' && c != '\n') { v *= 10; v += c - '0'; } }
+inline void read(string& s) { char c; while ((c = gc()) != EOF && c != '\n' && c != ' ') { s.push_back(c); } }
+inline void readline(string& s) { char c; while ((c = gc()) != EOF && c != '\n') { s.push_back(c); } }
+#else
+template <typename T> inline void read(T& a) { cin >> a; }
+template <typename T> inline void read(T& a, T& b) { cin >> a >> b; }
+template <typename T> inline void read(T& a, T& b, T& c) { cin >> a >> b >> c; }
+#endif
+#define quit cout << endl; _Exit(0);
+#define dread(type, a) type a; read(a)
+#define dread2(type, a, b) dread(type, a); dread(type, b)
+#define dread3(type, a, b, c) dread2(type, a, b); dread(type, c)
+#define dread4(type, a, b, c, d) dread3(type, a, b, c); dread(type, d)
+#define dread5(type, a, b, c, d, e) dread4(type, a, b, c, d); dread(type, e)
+#define readvector(type, name, size) vector<type> name(size); rep(i,size) {dread(type,temp); name[i]=temp;}
+#ifdef _DEBUG
+#define noop cout << "";
+#define deb __debugbreak();
+#define debassert(expr) if(!(expr)) deb;
+#define debif(expr) if(expr) deb;
+void print(const string& s) { cout << s << "\n"; }
+#else
+#define noop ;
+#define deb ;
+#define debif(expr) ;
+#define debassert(expr) ;
+void print(const string& s) {  }
+#endif
+
+#define rep(i, high) for (int i = 0; i < high; i++)
+#define repp(i, low, high) for (int i = low; i < high; i++)
+#define repe(i, container) for (auto& i : container)
+#define per(i, high) for (int i = high-1; i >= 0; i--)
+
+#define all(a) a.begin(),a.end()
+#define rall(a) a.rbegin(),a.rend()
+#define setcontains(set, x) (set.find(x) != set.end())
+#define sz(container) ((int)container.size())
+#define mp(a,b) (make_pair(a,b))
+
+#define ceildiv(x,y) ((x + y - 1) / y)
+
+template <typename T, typename U> inline void operator+=(pair<T, U>& l, const pair<T, U>& r) { l = { l.first + r.first,l.second + r.second }; }
+template <typename T, typename U> inline pair<T, U> operator+(const pair<T, U> l, const pair<T, U> r) { return { l.first + r.first, l.second + r.second }; }
+template <typename T, typename U> inline pair<T, U> operator-(const pair<T, U> l, const pair<T, U> r) { return { l.first - r.first, l.second - r.second }; }
+template <typename T, typename U> inline pair<T, U> operator*(const pair<T, U> l, const int m) { return { l.first * m, l.second * m }; }
+template <typename Out> inline void split(const string& s, char delim, Out result) { istringstream iss(s); string item; while (getline(iss, item, delim)) { *result++ = item; } }
+inline vector<string> split(const string& s, char delim) { vector<string> elems; split(s, delim, back_inserter(elems)); return elems; }
+vector<string> split(string s, string d) { size_t k = 0, n, e = d.length(); string t; vector<string> r; while ((n = s.find(d, k)) != string::npos) { t = s.substr(k, n - k); k = n + e; r.push_back(t); } r.push_back(s.substr(k)); return r; }
+ll binpow(ll a, ll b) { ll res = 1; while (b > 0) { if (b & 1) res = res * a; a = a * a; b >>= 1; } return res; }
+ll binpow(ll a, ll b, ll m) { a %= m; long long res = 1; while (b > 0) { if (b & 1) res = res * a % m; a = a * a % m; b >>= 1; } return res; } // For a < 2^31
+
+#if 1
+auto Start = chrono::high_resolution_clock::now();
+void resettimer() { Start = chrono::high_resolution_clock::now(); }
+int elapsedmillis() { return chrono::duration_cast<chrono::milliseconds>(chrono::high_resolution_clock::now() - Start).count(); }
+random_device rd;
+mt19937 rng(rd());
+template<typename T, typename U> inline int randint(T lo, U hi) { return uniform_int_distribution<int>((int)lo, (int)hi)(rng); } // [lo,hi]
+template<typename T> inline T randel(vector<T>& v) { return v[uniform_int_distribution<int>(int(0), int(v.size()) - int(1))(rng)]; } // [lo,hi]
+#endif
+const ll mod = 1e9 + 7;
+vp2 dirs = { {0,1},{0,-1},{1,0},{-1,0} };
+
+struct TwoSat {
+	int N;
+	vector<vi> gr;
+	vi values; // 0 = false, 1 = true
+
+	TwoSat(int n = 0) : N(n), gr(2 * n) {}
+
+	int addVar() { // (optional)
+		gr.emplace_back();
+		gr.emplace_back();
+		return N++;
+	}
+
+	void either(int f, int j) {
+		f = max(2 * f, -1 - 2 * f);
+		j = max(2 * j, -1 - 2 * j);
+		gr[f].push_back(j ^ 1);
+		gr[j].push_back(f ^ 1);
+	}
+	void setValue(int x) { either(x, x); }
+
+	void atMostOne(const vi& li) { // (optional)
+		if (sz(li) <= 1) return;
+		int cur = ~li[0];
+		repp(i, 2, sz(li)) {
+			int next = addVar();
+			either(cur, ~li[i]);
+			either(cur, next);
+			either(~li[i], next);
+			cur = ~next;
+		}
+		either(cur, ~li[1]);
+	}
+
+	vi val, comp, z; int time = 0;
+	int dfs(int i) {
+		int low = val[i] = ++time, x; z.push_back(i);
+		for (int e : gr[i]) if (!comp[e])
+			low = min(low, val[e] ? val[e] : dfs(e));
+		if (low == val[i]) do {
+			x = z.back(); z.pop_back();
+			comp[x] = low;
+			if (values[x >> 1] == -1)
+				values[x >> 1] = x & 1;
+		} while (x != i);
+		return val[i] = low;
+	}
+
+	bool solve() {
+		values.assign(N, -1);
+		val.assign(2 * N, 0); comp = val;
+		repp(i, 0, 2 * N) if (!comp[i]) dfs(i);
+		repp(i, 0, N) if (comp[2 * i] == comp[2 * i + 1]) return 0;
+		return 1;
+	}
+};
+
+int32_t main()
+{
+	fast();
+#if 0 && _LOCAL
+	ifstream instream("C:\\users\\matis\\Desktop\\comp_prog\\x64\\debug\\in.txt");
+	cin.rdbuf(instream.rdbuf());
+#endif
+
+	dread2(int, n, m);
+	TwoSat sat(m);
+	rep(i, n)
+	{
+		dread(char, a);
+		dread(int, ia); ia--;
+		if (a == '-') ia = ~ia;
+
+		dread(char, b);
+		dread(int, ib); ib--;
+		if (b == '-') ib = ~ib;
+		sat.either(ia, ib);
+	}
+
+	if (sat.solve())
+	{
+		rep(i, m)
+		{
+			if (sat.values[i]) cout << "+ ";
+			else cout << "- ";
+		}
+	}
+	else
+	{
+		cout << "IMPOSSIBLE";
+	}
+
+	quit;
+}
