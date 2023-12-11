42d583994ca2a8e08869ef224e040201f18bc844
Matistjati <matistjati@outlook.com>
2023-12-11 08:13:59 +0100

    Solved divisor game

---
+++ open/divisorgame.cpp
@@ -0,0 +1,245 @@
+#pragma GCC optimize("Ofast,inline,unroll-loops")
+#pragma GCC target("avx2,popcnt,lzcnt,abm,bmi,bmi2,fma,tune=native")
+
+#include <bits/stdc++.h>
+#include <bits/extc++.h>
+using namespace std;
+
+#define enablell 1
+
+typedef long long ll;
+typedef unsigned long long ull;
+#if enablell
+#define int ll
+#define inf int(1e18)
+#else
+const int inf = int(2e9);
+#endif
+typedef vector<ull> vull;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<vvi> vvvi;
+typedef vector<vvvi> vvvvi;
+typedef vector<bool> vb;
+typedef vector<vb> vvb;
+typedef vector<vvb> vvvb;
+typedef pair<int, int> p2;
+typedef vector<p2> vp2;
+typedef vector<vp2> vvp2;
+typedef vector<vvp2> vvvp2;
+typedef tuple<int, int, int> p3;
+typedef vector<p3> vp3;
+typedef vector<vp3> vvp3;
+typedef vector<vvp3> vvvp3;
+typedef tuple<int, int, int, int> p4;
+typedef vector<p4> vp4;
+
+#define rep(i, high) for (int i = 0; i < high; i++)
+#define repp(i, low, high) for (int i = low; i < high; i++)
+#define repe(i, container) for (auto& i : container)
+#define per(i, high) for (int i = high-1; i >= 0; i--)
+
+#define _LOCAL _MSC_VER > 0
+#if _LOCAL
+
+#define assert(x) debassert(x)
+#define popcount(x) __popcnt(x)
+uint32_t ctz(uint32_t x) { return _tzcnt_u32(x); }
+#else
+#define popcount(x) __builtin_popcountll(x)
+uint32_t ctz(uint32_t x) { return __builtin_ctzll(x); }
+
+#if 0
+namespace pbds
+{
+    using namespace __gnu_pbds;
+
+    template<typename T> using indexed_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+    template<typename T, typename U> using indexed_map = tree<T, U, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+    struct chash { // large odd number for C
+        const uint64_t C = ll(4e18 * acos(0)) | 71;
+        ll operator()(ll x) const { return __builtin_bswap64(x * C); }
+    };
+    template<typename T, typename U> using fast_map = __gnu_pbds::gp_hash_table<T, U, chash>;
+    template<typename T> using fast_set = __gnu_pbds::gp_hash_table<T, null_type, chash>;
+    template<typename T, typename H> using fast_set_h = __gnu_pbds::gp_hash_table<T, null_type, H>;
+}
+using namespace pbds;
+#endif
+#endif
+
+inline void fast() { ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); }
+#define FAST_INPUT 0
+#if FAST_INPUT && !_LOCAL
+#define gc() getchar_unlocked()
+inline void read(int& v) { v = 0; int sign = 1; char c = gc(); if (c == '-') { sign = -1; } else { v += c - '0'; } while ((c = gc()) && c != ' ' && c != '\n') { if (c == EOF) { v = -1; return; } v *= 10; v += c - '0'; } v *= sign; }
+inline void read(int& u, int& v) { read(u); read(v); }
+inline void read(int& u, int& v, int& k) { read(u); read(v); read(k); }
+//inline void read(int& v) { char c; while ((c = getchar()) != EOF && c != ' ' && c != '\n') { v *= 10; v += c - '0'; } }
+inline void read(string& s) { char c; while ((c = gc()) != EOF && c != '\n' && c != ' ') { s.push_back(c); } }
+inline void readline(string& s) { char c; while ((c = gc()) != EOF && c != '\n') { s.push_back(c); } }
+#else
+template <typename T> inline void read(T& a) { cin >> a; }
+inline void read(p2& a) { cin >> a.first >> a.second; }
+#endif
+
+#define quit cout << flush; _Exit(0);
+void readinput() {} // Recursion base case
+template<typename T, typename... Args> void readinput(T& arg, Args&... args) { read(arg); readinput(args...); }
+#define dread(type, ...) type __VA_ARGS__; readinput(__VA_ARGS__);
+template<typename T> istream& operator>>(istream& is, vector<T>& v) { for (T& u : v) read(u); return is; }
+#define _ << " " <<
+
+#ifdef _DEBUG
+#define noop cout << "";
+#define deb __debugbreak();
+#define debassert(expr) if(!(expr)) deb;
+#define debif(expr) if(expr) deb;
+#else
+#define noop ;
+#define deb ;
+#define debif(expr) ;
+#define debassert(expr) ;
+#endif
+
+#define all(a) a.begin(),a.end()
+#define rall(a) a.rbegin(),a.rend()
+#define setcontains(set, x) (set.find(x) != set.end())
+#define sz(container) ((int)container.size())
+#define mp(a,b) (make_pair(a,b))
+
+#define ceildiv(x,y) ((x + y - 1) / y)
+ll binpow(ll a, ll b) { ll res = 1; while (b > 0) { if (b & 1) res = res * a; a = a * a; b >>= 1; } return res; }
+ll binpow(ll a, ll b, ll m) { a %= m; long long res = 1; while (b > 0) { if (b & 1) res = res * a % m; a = a * a % m; b >>= 1; } return res; } // For a < 2^31
+
+template <typename T, typename U> inline void operator+=(pair<T, U>& l, const pair<T, U>& r) { l = { l.first + r.first,l.second + r.second }; }
+template <typename T, typename U> inline pair<T, U> operator+(const pair<T, U> l, const pair<T, U> r) { return { l.first + r.first, l.second + r.second }; }
+template <typename T, typename U> inline pair<T, U> operator-(const pair<T, U> l, const pair<T, U> r) { return { l.first - r.first, l.second - r.second }; }
+template <typename T, typename U> inline pair<T, U> operator*(const pair<T, U> l, const int m) { return { l.first * m, l.second * m }; }
+vector<string> split(string s, string d) { size_t k = 0, n, e = d.length(); string t; vector<string> r; while ((n = s.find(d, k)) != string::npos) { t = s.substr(k, n - k); k = n + e; r.push_back(t); } r.push_back(s.substr(k)); return r; }
+
+#if 1
+auto Start = chrono::high_resolution_clock::now();
+void resettimer() { Start = chrono::high_resolution_clock::now(); }
+int elapsedmillis() { return chrono::duration_cast<chrono::milliseconds>(chrono::high_resolution_clock::now() - Start).count(); }
+random_device rd;
+mt19937 rng(rd());
+template<typename T, typename U> inline int randint(T lo, U hi) { return uniform_int_distribution<int>((int)lo, (int)hi)(rng); } // [lo,hi]
+template<typename T> inline T randel(vector<T>& v) { return v[uniform_int_distribution<int>(int(0), int(v.size()) - int(1))(rng)]; } // [lo,hi]
+#endif
+const ll mod = 1e9 + 7;
+vp2 dirs = { {0,1},{0,-1},{1,0},{-1,0} };
+
+const int MAX_PR = 1'000'100;
+bitset<MAX_PR> isprime;
+vi eratosthenesSieve(int lim = MAX_PR - 1) {
+    isprime.set(); isprime[0] = isprime[1] = 0;
+    for (int i = 4; i < lim; i += 2) isprime[i] = 0;
+    for (int i = 3; i * i < lim; i += 2) if (isprime[i])
+        for (int j = i * i; j < lim; j += i * 2) isprime[j] = 0;
+    vi pr;
+    repp(i, 2, lim) if (isprime[i]) pr.push_back(i);
+    return pr;
+}
+
+vi pre(int(1e5));
+void precomp()
+{
+    rep(i, pre.size())
+    {
+        pre[i] = binpow(2, i - 1, mod);
+    }
+}
+
+inline void update(int& x, int b)
+{
+    x = (x + b) % mod;
+}
+
+int smarterdp(vp2& freq)
+{
+    int largestkey = 0;
+    rep(i, freq.size()) largestkey = max(largestkey, freq[i].first);
+    vvi dp(2, vi(largestkey * 3));
+    vvi dp2(2, vi(dp[0].size()));
+    dp[0][0] = 1;
+
+    rep(l, freq.size())
+    {
+        int i = freq[l].first;
+        if (!i) continue;
+        int k = pre[freq[l].second];
+
+
+        dp2 = dp;
+        rep(j, dp[0].size())
+        {
+            rep(m, 2)
+            {
+                int v = m;
+                if (i > 1) v = 1;
+
+                int val = dp[m][j];
+                // place odd number
+
+                if ((i ^ j) < dp[0].size()) update(dp2[v][i ^ j], val * k % mod);
+
+                // place even number
+                update(dp2[v][j], val * (k - 1 + mod) % mod);
+            }
+        }
+        swap(dp, dp2);
+    }
+
+    return (dp[0][1] + dp[1][0]) % mod;
+}
+
+int32_t main()
+{
+    fast();
+#if 0 && _LOCAL
+    ifstream instream("C:\\users\\matis\\source\\repos\\comp_prog\\x64\\debug\\in.txt");
+    cin.rdbuf(instream.rdbuf());
+#endif
+
+    precomp();
+    vi primes = eratosthenesSieve();
+
+    int tot = 0;
+    int numzero = 0;
+
+    dread(int, t);
+    while (t--)
+    {
+        dread(int, l, r);
+
+
+        vi nums(r - l + 1);
+        vi grundys(r - l + 1, 0);
+        repp(i, l, r + 1) nums[i - l] = i;
+        repe(p, primes)
+        {
+            int s = (l / p) * p;
+            if (s < l) s += p;
+            while (s <= r)
+            {
+                int k = 0;
+                while (nums[s - l] % p == 0) nums[s - l] /= p, k++;
+                grundys[s - l] += k;
+                s += p;
+            }
+        }
+        rep(i, r - l + 1) if (nums[i] > 1) grundys[i]++;
+
+        int hi = *max_element(all(grundys));
+        map<int, int> freq;
+        repe(v, grundys) freq[v]++;
+        vp2 pairs;
+        repe(v, freq) pairs.emplace_back(v);
+
+        cout << smarterdp(pairs) << "\n";
+    }
+
+    quit;
+}
