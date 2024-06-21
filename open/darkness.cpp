5a40fb14e2c16f1c30f08ff349b7cb6c12fbe74e
matistjati <matistjati@outlook.com>
2024-06-21 04:06:03 +0200

    Solved some max flow problems

---
+++ open/darkness.cpp
@@ -0,0 +1,142 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+#define int ll
+const int inf = int(1e18);
+
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int, int> p2;
+
+#define rep(i, high) for (int i = 0; i < high; i++)
+#define repp(i, low, high) for (int i = low; i < high; i++)
+#define repe(i, container) for (auto& i : container)
+#define sz(container) ((int)container.size())
+#define all(x) begin(x),end(x)
+#define ceildiv(x,y) (((x) + (y) - 1) / (y))
+
+inline void fast() { ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); }
+
+#if _LOCAL
+#define assert(x) if (!(x)) __debugbreak()
+#endif
+
+
+struct PushRelabel {
+	struct Edge {
+		int dest, back;
+		ll f, c;
+	};
+	vector<vector<Edge>> g;
+	vector<ll> ec;
+	vector<Edge*> cur;
+	vector<vi> hs; vi H;
+	PushRelabel(int n) : g(n), ec(n), cur(n), hs(2 * n), H(n) {}
+
+	void addEdge(int s, int t, ll cap, ll rcap = 0) {
+		if (s == t) return;
+		g[s].push_back({ t, sz(g[t]), 0, cap });
+		g[t].push_back({ s, sz(g[s]) - 1, 0, rcap });
+	}
+
+	void addFlow(Edge& e, ll f) {
+		Edge& back = g[e.dest][e.back];
+		if (!ec[e.dest] && f) hs[H[e.dest]].push_back(e.dest);
+		e.f += f; e.c -= f; ec[e.dest] += f;
+		back.f -= f; back.c += f; ec[back.dest] -= f;
+	}
+	ll calc(int s, int t) {
+		int v = sz(g); H[s] = v; ec[t] = 1;
+		vi co(2 * v); co[0] = v - 1;
+		repp(i, 0, v) cur[i] = g[i].data();
+		for (Edge& e : g[s]) addFlow(e, e.c);
+
+		for (int hi = 0;;) {
+			while (hs[hi].empty()) if (!hi--) return -ec[s];
+			int u = hs[hi].back(); hs[hi].pop_back();
+			while (ec[u] > 0)  // discharge u
+				if (cur[u] == g[u].data() + sz(g[u])) {
+					H[u] = 1e9;
+					for (Edge& e : g[u]) if (e.c && H[u] > H[e.dest] + 1)
+						H[u] = H[e.dest] + 1, cur[u] = &e;
+					if (++co[H[u]], !--co[hi] && hi < v)
+						repp(i, 0, v) if (hi < H[i] && H[i] < v)
+						--co[H[i]], H[i] = v + 1;
+					hi = H[u];
+				}
+				else if (cur[u]->c && H[u] == H[cur[u]->dest] + 1)
+					addFlow(*cur[u], min(ec[u], cur[u]->c));
+				else ++cur[u];
+		}
+	}
+	bool leftOfMinCut(int a) { return H[a] >= sz(g); }
+};
+
+signed main()
+{
+	fast();
+
+	int b;
+	int h;
+	int r, c;
+	cin >> b >> h >> r >> c;
+
+	vvi lights(r, vi(c));
+	rep(i, r)
+	{
+		string row;
+		cin >> row;
+		rep(j, c) lights[i][j] = row[j] - '0';
+	}
+
+	vector<vector<double>> light_level(r, vector<double>(c));
+
+	rep(i, r)
+	{
+		rep(j, c)
+		{
+			rep(a, r)
+			{
+				rep(b, c)
+				{
+					double dist = pow(i - a, 2) + pow(j - b, 2) + pow(h, 2);
+					light_level[a][b] += lights[i][j] / dist;
+				}
+			}
+		}
+	}
+
+	int numnodes = r * c + 2;
+	PushRelabel flow(numnodes);
+
+
+	vector<p2> dirs = { {0,1},{0,-1},{1,0},{-1,0} };
+	rep(i, r)
+	{
+		rep(j, c)
+		{
+			bool currlit = light_level[i][j] > b;
+			if (currlit)
+			{
+				if (i==0||j==0||i==r-1||j==c-1) flow.addEdge(i + j * r, numnodes - 1, inf);
+			}
+			else flow.addEdge(numnodes - 2, i + j * r, inf);
+			repe(dir, dirs)
+			{
+				p2 np = p2(i + dir.first, j + dir.second);
+				if (np.first < 0 || np.second < 0 || np.first >= r || np.second >= c) continue;
+				bool newlit = light_level[np.first][np.second] > b;
+				//assert(!(newlit && currlit));
+				if (newlit && currlit)
+				{
+					flow.addEdge(i + j * r, np.first + np.second * r, 43);
+				}
+				else flow.addEdge(i + j * r, np.first + np.second * r, 11);
+			}
+		}
+	}
+	cout << flow.calc(numnodes - 2, numnodes - 1);
+
+	return 0;
+}
