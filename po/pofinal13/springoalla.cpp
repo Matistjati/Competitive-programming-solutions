319e2a648b769cbce64a732b3c8e3003fe755fb4
Matistjati <matistjati@outlook.com>
2025-01-02 20:01:05 +0100

    [springoalla] faster sol

--- po/pofinal13/springoalla.cpp
+++ po/pofinal13/springoalla.cpp
@@ -1,151 +1,112 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-#pragma GCC target ("avx2")
-#pragma GCC optimization ("O3")
-#pragma GCC optimization ("unroll-loops")
+//typedef long long ll;
+//#define int ll
+const int inf = int(1e9);
 
-#define enablell 0
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int, int> p2;
 
-#define ll long long
-#if enablell
-#define int ll
-#endif
-#define vi vector<int>
-#define vvi vector<vi>
-#define vvvi vector<vvi>
-#define p2 pair<int, int>
-#define p3 tuple<int,int,int>
-#define p4 tuple<int,int,int,int>
-#define vp2 vector<p2>
-#define vp3 vector<p3>
-#if enablell
-#define inf 1e18
-#else
-#define inf int(1e9)
-#endif
-
-#define read(a) cin >> a
-#define write(a) cout << (a) << "\n"
-#define quit cout << endl; _Exit(0);
-#define dread(type, a) type a; cin >> a
-#define dread2(type, a, b) dread(type, a); dread(type, b)
-#define dread3(type, a, b, c) dread2(type, a, b); dread(type, c)
-#define dread4(type, a, b, c, d) dread3(type, a, b, c); dread(type, d)
-#define dread5(type, a, b, c, d, e) dread4(type, a, b, c, d); dread(type, e)
-#ifdef _DEBUG
-#define deb __debugbreak();
-#else
-#define deb ;
-#endif
-
-#define rep(i, high) for (ll i = 0; i < high; i++)
-#define repp(i, low, high) for (ll i = low; i < high; i++)
+#define rep(i, high) for (int i = 0; i < (high); i++)
+#define repp(i, low, high) for (int i = (low); i < (high); i++)
 #define repe(i, container) for (auto& i : container)
-#define per(i, high) for (ll i = high; i >= 0; i--)
-
-#define readpush(type,vect) type temp; read(temp); vect.push_back(temp);
-#define readvector(type, name, size) vector<type> name(size); rep(i,size) {dread(type,temp); name[i]=temp;}
-#define readinsert(type,a) {type temp; read(temp); a.insert(temp);}
-#define all(a) begin(a),end(a)
-#define setcontains(set, x) (set.find(x) != set.end())
-#define stringcontains(str, x) (str.find(x) != string::npos)
-#define within(a, b, c, d) (a >= 0 && a < b && c >= 0 && c < d)
 #define sz(container) ((int)container.size())
+#define all(x) begin(x),end(x)
 
-#define ceildiv(x,y) ((x + y - 1) / y)
-#define fract(a) (a-floor(a))
-#define sign(a) ((a>0) ? 1 : -1)
+inline void fast() { cin.tie(0)->sync_with_stdio(0); }
 
-auto Start = chrono::high_resolution_clock::now();
-
-inline void fast()
-{
-    ios::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
-}
-
-#define anstype tuple<int,int,vector<int>>
+#if _LOCAL
+#define assert(x) if (!(x)) __debugbreak()
+#endif
 
 
-int32_t main()
+signed main()
 {
     fast();
 
-#if 0
-    ifstream cin("C:\\Users\\Matis\\source\\repos\\Comp prog\\x64\\Debug\\in.txt");
-#endif
+    int n, t;
+    cin >> n >> t;
+    
+    vi trails(n);
 
-    dread2(int, n, t);
-
-    vi memo(t + 40001, inf);
-    vp2 trails(n*2);
-    vp3 extraTrails(n*2);
     rep(i, n)
     {
-        dread(int, trail);
-        trails[i*2] = {trail,1};
-        trails[i*2+1] = {trail + trail / 2,2};
-        extraTrails[i*2] = {trail,1,i};
-        extraTrails[i*2+1] = {trail + trail / 2,2,i};
+        int tlen;
+        cin >> tlen;
+        trails[i] = tlen;
     }
 
+    // sorting makes the memory accesses slightly more structured
+    vi trailsc(trails);
+    sort(all(trailsc));
+
+    vi memo(t+1, inf);
     memo[0] = 0;
-
-    repp(i, 1, memo.size())
+    
+    repe(tlen, trailsc)
     {
-        repe(j, trails)
+        repp(i, tlen, memo.size())
         {
-            if (i - j.first >= 0)
+            memo[i] = min(memo[i], memo[i - tlen] + 1);
+        }
+    }
+    repe(tlen, trailsc)
+    {
+        int twice_time = tlen + tlen / 2;
+        repp(i, twice_time, memo.size())
+        {
+            memo[i] = min(memo[i], memo[i - twice_time] + 2);
+        }
+    }
+    
+    p2 best = p2(inf, inf);
+    rep(time, sz(memo))
+    {
+        if (memo[time] == inf) continue;
+        // time + x >= t
+        // x >= t-time
+        auto it = lower_bound(all(trailsc), t - time);
+        if (it != trailsc.end()) best = min(best, p2(time + *it, memo[time] + 1));
+    }
+    
+    rep(time, sz(memo))
+    {
+        if (memo[time] == inf) continue;
+        // time + x*1.5 >= t
+        // x >= (t-time)/1.5
+        // x >= 2*(t-time)/3
+        auto it = lower_bound(all(trailsc), (2*(t - time)+2)/3);
+        if (it != trailsc.end()) best = min(best, p2(time + (*it+*it/2), memo[time] + 2));
+    }
+    
+    int time = best.first;
+    int numruns = best.second;
+    cout << time << " " << numruns << "\n";
+
+    vi runs(n);
+    while (time > 0)
+    {
+        rep(j, n)
+        {
+            int tlen = trails[j];
+            if (time - tlen >= 0 && memo[time - tlen] == numruns -1)
             {
-                if (memo[i - j.first] + j.second < memo[i])
-                {
-                    memo[i] = memo[i - j.first] + j.second;
-                }
+                runs[j] += tlen;
+                time -= tlen;
+                numruns--;
+            }
+            int twice_time = tlen + tlen / 2;
+            if (time - twice_time >= 0 && memo[time - twice_time] == numruns - 2)
+            {
+                runs[j] += twice_time;
+                time -= twice_time;
+                numruns -= 2;
             }
         }
     }
+    repe(u, runs) cout << u << " ";
 
-    repp(i, t, memo.size())
-    {
-        int p = memo[i];
-        if (memo[i] != inf)
-        {
-            cout << i << " " << memo[i] << "\n";
-        }
-        else
-        {
-            continue;
-        }
-
-        vi runs(n);
-        while (i > 0)
-        {
-            repe(j, extraTrails)
-            {
-                int time;
-                int cost;
-                int index;
-                tie(time, cost, index) = j;
-                if (i-time>=0&&memo[i-time] == p-cost)
-                {
-                    runs[index] += time;
-                    p-=cost;
-                    i -= time;
-                    break;
-                }
-            }
-        }
-        repe(j, runs)
-        {
-            cout << j << " ";
-        }
-        break;
-    }
-
-
-
-
-    quit;
+    return 0;
 }
