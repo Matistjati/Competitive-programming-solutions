8eefcedb49a7248eee7f50af382ad621dac38912
Matistjati <matistjati@outlook.com>
2025-01-08 04:21:10 +0100

    Fastest solution on an easy array problem

--- open/aneasyarray.cpp
+++ open/aneasyarray.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 
 typedef long long ll;
-#define int ll
-const int inf = int(1e18);
+//#define int ll
+
 
 typedef vector<int> vi;
 typedef vector<vi> vvi;
@@ -21,202 +21,200 @@ inline void fast() { ios::sync_with_stdio(0); cin.tie(0); }
 #define assert(x) if (!(x)) __debugbreak()
 #endif
 
-struct mintree {
-	typedef p2 T;
-	static constexpr T unit = p2(inf, inf);
-	T f(T a, T b)
-	{
-		int min1 = inf, min2 = inf;
-
-		for (int val : {a.first, a.second, b.first, b.second}) {
-			if (val < min1) {
-				min2 = min1; // update second largest
-				min1 = val;  // update largest
-			}
-			else if (val < min2) {
-				min2 = val;  // update second largest if it's not the largest
-			}
-		}
-
-		return p2(min1, min2);
-	} // (any associative fn)
-	vector<T> s; int n;
-	mintree(int n = 0, T def = unit) : s(2 * n, def), n(n) {}
-	void update(int pos, T val) {
-		for (s[pos += n] = val; pos /= 2;)
-			s[pos] = f(s[pos * 2], s[pos * 2 + 1]);
-	}
-	T query(int b, int e) { // query [b, e)
-		e++;
-		T ra = unit, rb = unit;
-		for (b += n, e += n; b < e; b /= 2, e /= 2) {
-			if (b % 2) ra = f(ra, s[b++]);
-			if (e % 2) rb = f(s[--e], rb);
-		}
-		return f(ra, rb);
-	}
-};
-struct maxtree {
-	typedef p2 T;
-	static constexpr T unit = p2(-inf, -inf);
-	T f(T a, T b)
-	{
-		int max1 = -inf, max2 = -inf;
-
-		for (int val : {a.first, a.second, b.first, b.second}) {
-			if (val > max1) {
-				max2 = max1; // update second largest
-				max1 = val;  // update largest
-			}
-			else if (val > max2) {
-				max2 = val;  // update second largest if it's not the largest
-			}
-		}
-
-		return p2(max1, max2);
-	} // (any associative fn)
-	vector<T> s; int n;
-	maxtree(int n = 0, T def = unit) : s(2 * n, def), n(n) {}
-	void update(int pos, T val) {
-		for (s[pos += n] = val; pos /= 2;)
-			s[pos] = f(s[pos * 2], s[pos * 2 + 1]);
-	}
-	T query(int b, int e) { // query [b, e)
-		e++;
-		T ra = unit, rb = unit;
-		for (b += n, e += n; b < e; b /= 2, e /= 2) {
-			if (b % 2) ra = f(ra, s[b++]);
-			if (e % 2) rb = f(s[--e], rb);
-		}
-		return f(ra, rb);
-	}
-};
-
-#define FAST_INPUT 1
-#if FAST_INPUT && !_LOCAL
-#define gc() getchar_unlocked()
-inline void read(int& v) { v = 0; int sign = 1; char c = gc(); if (c == '-') { sign = -1; } else { v += c - '0'; } while ((c = gc()) && c != ' ' && c != '\n') { if (c == EOF) { v = -1; return; } v *= 10; v += c - '0'; } v *= sign; }
-inline void read(int& u, int& v) { read(u); read(v); }
-inline void read(int& u, int& v, int& k) { read(u); read(v); read(k); }
-//inline void read(int& v) { char c; while ((c = getchar()) != EOF && c != ' ' && c != '\n') { v *= 10; v += c - '0'; } }
-inline void read(string& s) { char c; while ((c = gc()) != EOF && c != '\n' && c != ' ') { s.push_back(c); } }
-inline void readline(string& s) { char c; while ((c = gc()) != EOF && c != '\n') { s.push_back(c); } }
+#if !_LOCAL
+// NOLINTBEGIN
+// clang-format off
+// DO NOT REMOVE THIS MESSAGE. The mess that follows is a minified build of
+// https://github.com/purplesyringa/blazingio. Refer to the repository for
+// a human-readable version and documentation.
+// Options: cbfoiedrhWLMXaIaAn
+#define M$(x,...)_mm256_##x##_epi8(__VA_ARGS__)
+#define $u(...)__VA_ARGS__
+#if __APPLE__
+#define $m(A,B)A
 #else
-template <typename T> inline void read(T& a) { cin >> a; }
-inline void read(p2& a) { cin >> a.first >> a.second; }
+#define $m(A,B)B
+#endif
+#if _WIN32
+#define $w(A,B)A
+#else
+#define $w(A,B)B
+#endif
+#if __i386__|_M_IX86
+#define $H(A,B)A
+#else
+#define $H(A,B)B
+#endif
+#if __aarch64__
+#define $a(A,B)A
+#else
+#define $a(A,B)B
+#endif
+#define $P(x)void F(x K){
+#define $T template<$c T
+#define $c class
+#define $C constexpr
+#define $R return
+#define $O operator
+#define u$ uint64_t
+#define $r $R*this;
+#include<array>
+#include<bitset>
+#include<complex>
+#include<cstring>
+#include $a(<arm_neon.h>,<immintrin.h>)
+#include<stdint.h>
+#include $w(<windows.h>,<sys/mman.h>)
+#include<sys/stat.h>
+#include $w(<io.h>,<unistd.h>)
+#include $w(<ios>,<sys/resource.h>)
+#if _MSC_VER
+#define __builtin_add_overflow(a,b,c)_addcarry_u64(0,a,b,c)
+#define $s
+#else
+$H(, u$ _umul128(u$ a, u$ b, u$* D) { auto x = (__uint128_t)a * b; *D = u$(x >> 64); $R(u$)x; })
+#define $s $a(,__attribute__((target("avx2"))))
+#endif
+#define $z $a(16,32)
+#define $t $a(uint8x16_t,__m256i)
+#define $I $w(__forceinline,__attribute__((always_inline)))
+#define $F M(),
+#define E$(x)if(!(x))abort();
+$w(LONG WINAPI $x(_EXCEPTION_POINTERS*); , )namespace $f {
+	using namespace std; struct B { enum $c A :char {}c; B& $O = (char x){ c = A{x}; $r }$O char() { $R(char)c; } }; $C u$ C = ~0ULL / 255; struct D { string& K; }; static B E[65568]; template<int F>struct G { B* H, * S; void K(off_t C) { $w(char* D = (char*)VirtualAlloc(0, (C + 8191) & -4096, 8192, 1); E$(D)E$(VirtualFree(D, 0, 32768))DWORD A = C & -65536; E$(!A || MapViewOfFileEx(CreateFileMapping(GetStdHandle(-10), 0, 2, 0, A, 0), 4, 0, 0, 0, D) == D)E$(VirtualAlloc(D + A, 65536, 12288, 4) == D + A)E$(~_lseek(0, A, 0))DWORD E = 0; ReadFile(GetStdHandle(-10), D + A, 65536, &E, 0); , int A = getpagesize(); char* D = (char*)mmap(0, C + A, 3, 2, 0, 0); E$(D != (void*)-1)E$(mmap(D + ((C + A - 1) & -A), A, 3, $m(4114, 50), -1, 0) != (void*)-1))H = (B*)D + C; *H = 10; H[1] = 48; H[2] = 0; S = (B*)D; }void L() { H = S = E; }$I void M() { if (F && S == H) { $w(DWORD A = 0; ReadFile(GetStdHandle(-10), S = E, 65536, &A, 0); , $a($u(register long A asm("x0") = 0, D asm("x1") = (long)E, G asm("x2") = 65536, C asm($m("x16", "x8")) = $m(3, 63); asm volatile("svc 0" $m("x80", ) :"+r"(A), "+r"(D) : "r"(C), "r"(G)); S = launder(E);), off_t A = $H(3, $m(33554435, 0)); B * D = E; asm volatile($H("int $128", "syscall") :"+a"(A), $H("+c"(D) : "b", "+S"(D) : "D")(0), "d"(65536)$H(, $u(:"rcx", "r11"))); S = D;))H = S + A; *H = 10; if (!A)E[1] = 48, E[2] = 0; } }$T > $I void N(T& x) { while ($F(*S & 240) == 48)x = T(x * 10 + (*S++ - 48)); }$T > $I decltype((void)~T{ 1 })O(T& x) { M(); int A = is_signed_v<T> && *S == 45; S += A; N(x = 0); x = A ? 1 + ~x : x; }$T > $I decltype((void)T{ 1. })O(T& x) { M(); int A = *S == 45; S += A; $F S += *S == 43; u$ n = 0; int i = 0; for (; i < 18 && ($F * S & 240) == 48; i++)n = n * 10 + *S++ - 48; int B = 20; int C = *S == 46; S += C; for (; i < 18 && ($F * S & 240) == 48; i++)n = n * 10 + *S++ - 48, B -= C; x = (T)n; while (($F * S & 240) == 48)x = x * 10 + *S++ - 48, B -= C; if (*S == 46)S++, C = 1; while (($F * S & 240) == 48)x = x * 10 + *S++ - 48, B -= C; int D; if ((*S | 32) == 101)S++, $F S += *S == 43, O(D), B += D; static $C auto E = []() {array<T, 41>E{}; T x = 1; for (int i = 21; i--;)E[40 - i] = x, E[i] = 1 / x, x *= 10; $R E; }(); while (B > 40)x *= (T)1e10, B -= 10; while (B < 0)x *= (T)1e-10, B += 10; x *= E[B]; x = A ? -x : x; }$I void O(bool& x) { $F x = *S++ == 49; }$I void O(char& x) { $F x = *S++; }$I void O(uint8_t& x) { $F x = *S++; }$I void O(int8_t& x) { $F x = *S++; }$T > $s void P(string& K, T C) { M(); B* G = S; C(); K.assign((char*)G, S - G); while (F && S == H && ($F H != E)) { C(); K.append(E, S); } }$s void O(string& K) { P(K, [&]()$s{ B * p = S; $w(ULONG R; ,)$t x; $a(uint64x2_t A; while (memcpy(&x,p,16),A = uint64x2_t(x < 33),!(A[0] | A[1]))p += 16; S = p + (A[0] ? 0 : 8) + $w((_BitScanForward64(&R,A[0] ? A[0] : A[1]),R),__builtin_ctzll(A[0] ? A[0] : A[1])) / 8; ,int J; $t C = M$(set1,32); while (memcpy(&x,p,32),!(J = M$(movemask,M$(cmpeq,C,_mm256_max_epu8(C,x)))))p += 32; S = p + $w((_BitScanForward(&R,J),R),__builtin_ctz(J));) }); }$s void O(D& A) { P(A.K, [&]() {S = (B*)memchr(S, 10, H - S + 1); }); if (A.K.size() && A.K.back() == 13)A.K.pop_back(); if (A.K.empty() || S < H)S += *S == 10; }$T > $I void O(complex<T>& K) { T A, B{}; if ($F * S == 40) { S++; O(A); if ($F * S++ == 44)Q(B), S++; } else O(A); K = { A,B }; }template<size_t N>$s void O(bitset<N>& K) { if (N > 4095 && !*this)$R; ptrdiff_t i = N; while (i)if ($F i % $z || H - S < $z)K[--i] = *S++ == 49; else { B* p = S; for (int64_t j = 0; j < min(i, H - S) / $z; j++) { i -= $z; $t x; memcpy(&x, p, $z); $a(auto B = (uint8x16_t)vdupq_n_u64(~2ULL / 254) & (48 - x); auto C = vzip_u8(vget_high_u8(B), vget_low_u8(B)); auto y = vaddvq_u16((uint16x8_t)vcombine_u8(C.val[0], C.val[1])); , u$ a = ~0ULL / 65025; auto y = $w(_byteswap_ulong, __builtin_bswap32)(M$(movemask, M$(shuffle, _mm256_slli_epi32(x, 7), _mm256_set_epi64x(a + C * 24, a + C * 16, a + C * 8, a))));)p += $z; memcpy((char*)&K + i / 8, &y, $z / 8); }S = p; } }$T > $I void Q(T& K) { if (!is_same_v<T, D>)while ($F(uint8_t) * S < 33)S++; O(K); }$O bool() { $R!!*this; }bool $O!() { $R S > H; } }; struct U { G<0>A; G<1>B; U() { struct stat D; E$(~fstat(0, &D))(D.st_mode >> 12) == 8 ? A.K(D.st_size) : B.L(); }U* tie(nullptr_t) { $R this; }void sync_with_stdio(bool) {}$T > $I U& $O >> (T& K) { A.S ? A.Q(K) : B.Q(K); $r }$O bool() { $R!!*this; }bool $O!() { $R A.S ? !A : !B; } }; short A[100]; char L[64]{ 1 }; struct
+		V {
+		char* D; B* S; int J; V() { $w(E$(D = (char*)VirtualAlloc(0, 536870912, 8192, 4))E$(VirtualAlloc(D, 4096, 4096, 260))AddVectoredExceptionHandler(1, $x); , size_t C = 536870912; $m(, rlimit E; getrlimit(RLIMIT_AS, &E); if (~E.rlim_cur)C = 25165824;)D = (char*)mmap(0, C, 3, $m(4162, 16418), -1, 0); E$(D != (void*)-1))S = (B*)D; for (int i = 0; i < 100; i++)A[i] = short((48 + i / 10) | ((48 + i % 10) << 8)); for (int i = 1; i < 64; i++)L[i] = L[i - 1] + (0x8922489224892249 >> i & 1); }~V() { flush($w(!J, )); }void flush($w(int F = 0, )) { $w(J = 1; auto E = GetStdHandle(-11); auto C = F ? ReOpenFile(E, 1073741824, 7, 2684354560) : (void*)-1; DWORD A; E$(C == (void*)-1 ? WriteFile(E, D, DWORD((char*)S - D), &A, 0) : (WriteFile(C, D, DWORD(((char*)S - D + 4095) & -4096), &A, 0) && ~_chsize(1, int((char*)S - D)))), auto G = D; ssize_t A; while ((A = write(1, G, (char*)S - G)) > 0)G += A; E$(~A))S = (B*)D; }$P(char)* S++ = K;
+	}$P(uint8_t)* S++ = K;
+}$P(int8_t)* S++ = K;
+}$P(bool)* S++ = 48 + K; }$T > decltype((void)~T{ 1 })F(T K) { using D = make_unsigned_t<T>; D C = K; if (K < 0)F('-'), C = 1 + ~C; static $C auto N = []() {array<D, 5 * sizeof(T) / 2>N{}; D n = 1; for (size_t i = 1; i < N.size(); i++)n *= 10, N[i] = n; $R N; }(); $w(ULONG M; , )int G = L[$w(($H(_BitScanReverse(&M, ULONG((int64_t)C >> 32)) ? M += 32 : _BitScanReverse(&M, (ULONG)C | 1), _BitScanReverse64(&M, C | 1)), M), 63 ^ __builtin_clzll(C | 1))]; G -= C < N[G - 1]; short H[20]; if $C(sizeof(T) == 2) { auto n = 33555U * C - C / 2; u$ H = A[n >> 25]; n = (n & 33554431) * 25; H |= A[n >> 23] << 16; H |= u$(48 + ((n & 8388607) * 5 >> 22)) << 32; H >>= 40 - G * 8; memcpy(S, &H, 8); } else if $C(sizeof(T) == 4) { auto n = 1441151881ULL * C; $H(n >>= 25; n++; for (int i = 0; i < 5; i++) { H[i] = A[n >> 32]; n = (n & ~0U) * 100; }, int K = 57; auto J = ~0ULL >> 7; for (int i = 0; i < 5; i++) { H[i] = A[n >> K]; n = (n & J) * 25; K -= 2; J /= 4; })memcpy(S, (B*)H + 10 - G, 16); } else { $H($u(if (C < (1ULL << 32)) { $R F((uint32_t)C); }auto J = (u$)1e10; auto x = C / J, y = C % J; int K = 100000, b[]{ int(x / K),int(x % K),int(y / K),int(y % K) }; B H[40]; for (int i = 0; i < 4; i++) { int n = int((429497ULL * b[i] >> 7) + 1); B* p = H + i * 5; *p = 48 + char(n >> 25); n = (n & ~0U >> 7) * 25; memcpy(p + 1, A + (n >> 23), 2); memcpy(p + 3, A + ((n & ~0U >> 9) * 25 >> 21), 2); }), $u(u$ D, E = _umul128(18, C, &D), F; _umul128(0x725dd1d243aba0e8, C, &F); D += __builtin_add_overflow(E, F + 1, &E); for (int i = 0; i < 10; i++)H[i] = A[D], E = _umul128(100, E, &D);))memcpy(S, (B*)H + 20 - G, 20); }S += G; }$T > decltype((void)T{ 1. })F(T K) { if (K < 0)F('-'), K = -K; auto G = [&]() {auto x = u$(K * 1e12); $H($u(x -= x > 999999999999; uint32_t n[]{ uint32_t(x / 1000000 * 429497 >> 7) + 1,uint32_t(x % 1000000 * 429497 >> 7) + 1 }; int K = 25, J = ~0U >> 7; for (int i = 0; i < 3; i++) { for (int j = 0; j < 2; j++)memcpy(S + i * 2 + j * 6, A + (n[j] >> K), 2), n[j] = (n[j] & J) * 25; K -= 2; J /= 4; }S += 12;), $u(u$ D, E = _umul128(472236648287, x, &D) >> 8; E |= D << 56; D >>= 8; E++; for (int i = 0; i < 6; i++)memcpy(S, A + D, 2), S += 2, E = _umul128(100, E, &D);))}; if (K == 0)$R F('0'); if (K >= 1e16) { K *= (T)1e-16; int B = 16; while (K >= 1)K *= (T).1, B++; F("0."); G(); F('e'); F(B); } else if (K >= 1) { auto B = (u$)K; F(B); if ((K -= (T)B) > 0)F('.'), G(); } else F("0."), G(); }$P(const char*)$w(size_t A = strlen(K); memcpy((char*)S, K, A); S += A; , S = (B*)stpcpy((char*)S, K);)}$P(const uint8_t*)F((char*)K); }$P(const int8_t*)F((char*)K); }$P(string_view)memcpy(S, K.data(), K.size()); S += K.size(); }$T > $P(complex<T>)* this << '(' << K.real() << ',' << K.imag() << ')'; }template<size_t N>$s $P(const bitset<N>&)auto i = N; while (i % $z)*S++ = 48 + K[--i]; B* p = S; while (i) { i -= $z; $a(short, int)x; memcpy(&x, (char*)&K + i / 8, $z / 8); $a(auto A = (uint8x8_t)vdup_n_u16(x); vst1q_u8((uint8_t*)p, 48 - vtstq_u8(vcombine_u8(vuzp2_u8(A, A), vuzp1_u8(A, A)), (uint8x16_t)vdupq_n_u64(~2ULL / 254))); , auto b = _mm256_set1_epi64x(~2ULL / 254); _mm256_storeu_si256(($t*)p, M$(sub, M$(set1, 48), M$(cmpeq, _mm256_and_si256(M$(shuffle, _mm256_set1_epi32(x), _mm256_set_epi64x(0, C, C * 2, C * 3)), b), b)));)p += $z; }S = p; }$T > V& $O << (const T & K) { F(K); $r }V& $O << (V & (*A)(V&)) { $R A(*this); }}; struct W { $T > W& $O << (const T & K) { $r }W& $O << (W & (*A)(W&)) { $R A(*this); } }; }namespace std { $f::U i$; $f::V o$; $f::W e$; $f::U& getline($f::U& B, string& K) { $f::D A{ K }; $R B >> A; }$f::V& flush($f::V& B) { if (!i$.A.S)B.flush(); $R B; }$f::V& endl($f::V& B) { $R B << '\n' << flush; }$f::W& endl($f::W& B) { $R B; }$f::W& flush($f::W& B) { $R B; } }$w(LONG WINAPI $x(_EXCEPTION_POINTERS* A) { auto C = A->ExceptionRecord; auto B = C->ExceptionInformation[1]; if (C->ExceptionCode == 2147483649 && B - (ULONG_PTR)std::o$.D < 0x40000000) { E$(VirtualAlloc((char*)B, 16777216, 4096, 4) && VirtualAlloc((char*)(B + 16777216), 4096, 4096, 260))$R - 1; }$R 0; }, )
+#define freopen(...)if(freopen(__VA_ARGS__)==stdin)std::i$=$f::U{}
+#define cin i$
+#define cout o$
+#ifdef ONLINE_JUDGE
+#define cerr e$
+#define clog e$
+#endif
+// End of blazingio
+// NOLINTEND
+// clang-format on
 #endif
 
+vector<array<int, 3>> queries_sorted;
+// assumes queries to be [l,r]
+template<typename T, typename Monoid>
+vector<T> offlineRangeQueries(const vector<T>& arr, T identity)
+{
+	int n = arr.size();
+	int q = queries_sorted.size();
+
+	vector<T> fenwick(n + 1, identity);
+	vector<T> ans(q);
+
+	int qp = 0;
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = n - i; j < n + 1; j += (j & -j)) fenwick[j] = Monoid{}(fenwick[j], arr[i]);
+
+		while (qp < q&& queries_sorted[qp][1] == i)
+		{
+			T res = identity;
+			for (int j = n - queries_sorted[qp][0]; j > 0; j -= (j & -j)) res = Monoid{}(res, fenwick[j]);
+			ans[queries_sorted[qp][2]] = res;
+			qp++;
+		}
+	}
+	return ans;
+}
+
+typedef pair<short, short> el;
+const short inf = short((1 << 15)-1);
+struct maxquery
+{
+	el operator()(el a, el b)
+	{
+		return el(max(a.first, b.first), max(min(a.first, b.first), max(a.second, b.second)));
+	}
+};
+
+struct minquery
+{
+	el operator()(el a, el b)
+	{
+		return el(min(a.first, b.first), min(max(a.first, b.first), min(a.second, b.second)));
+	}
+};
 
 signed main()
 {
 	fast();
 
 	int n, q;
-	read(n), read(q);
+	cin >> n >> q;
 
 	vi nums(n);
-	rep(i, n) read(nums[i]);
-	gc();
+	rep(i, n) cin >> nums[i];
 
-	mintree posmin(n);
-	mintree negmin(n);
-	maxtree posmax(n);
-	maxtree negmax(n);
 
-	rep(i, n)
-	{
-		if (nums[i]>0)
+	vector<p2> queries(q);
+	rep(i, q) cin >> queries[i].first, cin >> queries[i].second, queries[i].second -= 2;
+
+	queries_sorted.resize(q);
+	for (int i = 0; i < q; i++) queries_sorted[i] = { queries[i].first, queries[i].second, i };
+	sort(queries_sorted.begin(), queries_sorted.end(), [](array<int, 3>& a, array<int, 3>& b)
 		{
-			posmin.update(i, p2(nums[i], inf));
-			posmax.update(i, p2(nums[i], -inf));
-		}
-		else if(nums[i]<0)
-		{
-			negmin.update(i, p2(-nums[i], inf));
-			negmax.update(i, p2(-nums[i], -inf));
-		}
-	}
+			return a[1] < b[1];
+		});
 
-	vi posp(n);
-	vi negp(n);
+	vector<el> numsp(n);
+	rep(i, n) numsp[i] = el(nums[i], inf);
+	vector<el> minqueries = offlineRangeQueries<el, minquery>(numsp, el(inf, inf));
+
+	rep(i, n) numsp[i] = el(nums[i], -inf);
+	vector<el> maxqueries = offlineRangeQueries<el, maxquery>(numsp, el(-inf, -inf));
+
 	vi zerop(n);
 	rep(i, n)
 	{
-		if (nums[i] > 0) posp[i]++;
-		if (nums[i] < 0) negp[i]++;
 		if (nums[i] == 0) zerop[i]++;
 	}
-	repp(i, 1, n) posp[i] += posp[i - 1];
-	repp(i, 1, n) negp[i] += negp[i - 1];
 	repp(i, 1, n) zerop[i] += zerop[i - 1];
 
 	auto sum = [](vi& pref, int l, int r)
-	{
-		if (l) l = pref[l - 1];
-		return pref[r] - l;
-	};
-
-	while (q--)
-	{
-		int l, r;
-		read(l);
-		read(r);
-		l--; r--;
-
-		int p = nums[l] * nums[r];
-		if (p==0)
 		{
-			cout << 0 << "\n";
-			continue;
-		}
-		l++;
-		r--;
-		int ans = -inf;
-		if (sum(zerop, l, r) > 0) ans = max(ans, 0LL);
-		if (p<0)
-		{
-			// largest positive, largest abs val negative
-			if (sum(posp, l, r) > 0 && sum(negp, l, r) > 0)
-			{
-				ans = max(ans, p * posmax.query(l, r).first * -negmax.query(l, r).first);
-			}
-			else if (ans<0)
-			{
-				// two positive or two negative
-				if (sum(posp, l, r) >= 2) // 2 smallest positive
-				{
-					p2 a = posmin.query(l, r);
-					ans = max(ans, p * a.first * a.second);
-				}
-				if (sum(negp, l, r) >= 2) // 2 smallest abs val negative
-				{
-					p2 a = negmin.query(l, r);
-					ans = max(ans, p * a.first * a.second);
-				}
-			}
-			
+			if (l) l = pref[l - 1];
+			return pref[r] - l;
+		};
 
-			
+	
+	rep(qind, q)
+	{
+		int l = queries[qind].first, r = queries[qind].second;
+
+		ll p = ll(nums[l - 1]) * nums[r + 1];
+
+		ll ans = -ll(1e18);
+		if (r < l + 7)
+		{
+			repp(i, l, r + 1)
+			{
+				repp(j, i + 1, r + 1) ans = max(ans, p * nums[i] * nums[j]);
+			}
 		}
 		else
 		{
-			// two positive or two negative
-			if (sum(posp,l,r)>=2) // 2 largest positive
-			{
-				p2 a = posmax.query(l, r);
-				ans = max(ans, p * a.first * a.second);
-			}
-			if (sum(negp,l,r)>=2) // 2 largest abs val negative
-			{
-				p2 a = negmax.query(l, r);
-				ans = max(ans, p * a.first * a.second);
-			}
-			
-			if (ans < 0 && sum(posp, l, r) > 0 && sum(negp, l, r) > 0) // smallest positive, smallest abs value negative
-			{
-				ans = max(ans, p * posmin.query(l, r).first * -negmin.query(l, r).first);
-			}
+			if (sum(zerop, l, r) > 0) ans = max(ans, 0LL);
+			p2 a = minqueries[qind];
+			p2 b = maxqueries[qind];
+			array<int, 4> nums;
+			int numsz = 0;
+			ans = max(ans, p * a.first * a.second);
+			ans = max(ans, p * a.first * b.first);
+			ans = max(ans, p * a.first * b.second);
+			ans = max(ans, p * a.second * b.second);
+			ans = max(ans, p * a.second * b.first);
+			ans = max(ans, p * b.second * b.first);
 		}
+
 		cout << ans << "\n";
 	}
 
