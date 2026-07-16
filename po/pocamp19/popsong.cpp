2855a48ace19cb7a2da2249bdde5b6b1ee4cc63e
Joshua Andersson <matistjati@outlook.com>
2026-07-16 03:34:44 +0200

    Clean up template in some solutions

--- po/pocamp19/popsong.cpp
+++ po/pocamp19/popsong.cpp
@@ -1,143 +1,134 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-typedef long long ll;
-#define int ll
-const int inf = int(1e18);
+using ll = long long;
+using vi = vector<ll>;
+using vvi = vector<vi>;
+using p2 = pair<ll, ll>;
+const ll inf = 1e18;
 
-typedef vector<int> vi;
-typedef vector<vi> vvi;
-typedef pair<int, int> p2;
+#define rep(i,n) for (ll i = 0; i < (n); i++)
+#define repp(i,a,n) for (ll i = (a); i < (n); i++)
+#define repe(i, arr) for (auto& i : arr)
+#define all(x) begin(x),end(x)
+#define sz(x) ((ll)(x).size())
 
-#define rep(i, high) for (int i = 0; i < high; i++)
-#define repp(i, low, high) for (int i = low; i < high; i++)
-#define repe(i, container) for (auto& i : container)
-#define per(i, high) for (int i = high-1; i >= 0; i--)
+const ll maxN = (ll)(2e5 + 1);
 
-#define _LOCAL _MSC_VER
-#if _LOCAL > 0
-#define deb __debugbreak();
-#define assert(x) debassert(x)
-#define popcount(x) __popcnt(x)
-#define clz(x) _lzcnt_u32(x)
-#else
-#define clz(x) __builtin_clz(x)
-#define deb ;
-#define popcount(x) __builtin_popcountll(x)
-#endif
+void fail() {
+    cout << 0 << "\n";
+    exit(0);
+}
 
-inline void fast() { ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); }
-
-#ifdef _DEBUG
-#define debassert(expr) if(!(expr)) deb;
-#else
-#define debassert(expr) ;
-#endif
-
-#define all(a) a.begin(),a.end()
-#define rall(a) a.rbegin(),a.rend()
-#define setcontains(set, x) (set.find(x) != set.end())
-#define sz(container) ((int)container.size())
-vector<p2> dirs = { {0,1},{0,-1},{1,0},{-1,0} };
-
-// time and rng
-auto Start = chrono::high_resolution_clock::now();
-void resettimer() { Start = chrono::high_resolution_clock::now(); }
-int elapsedmillis() { return chrono::duration_cast<chrono::milliseconds>(chrono::high_resolution_clock::now() - Start).count(); }
-random_device rd;
-mt19937 rng(rd());
-template<typename T, typename U> inline int randint(T lo, U hi) { return uniform_int_distribution<int>((int)lo, (int)hi)(rng); } // [lo,hi]
-template<typename T> inline T randel(vector<T>& v) { return v[uniform_int_distribution<int>(int(0), int(v.size()) - int(1))(rng)]; } // [lo,hi]
-
-signed main()
+using vvp2 = vector<vector<p2>>;
+ll getfurthestnode(vvp2& edges, bitset<maxN>& mustVisit, ll n, ll start)
 {
-	fast();
+    ll costiest = inf;
+    ll deepestNode = -1;
+    ll parent = start;
 
-	string s;
-	cin >> s;
-	reverse(all(s));
-	int n = sz(s);
-	set<p2> unique;
-	int calls = 0;
+    priority_queue<p2> pq;
+    pq.emplace(0, start);
+    vi seen(n);
 
-	vi lcps(n);
-	vi q(n);
-	vi next(n);
+    while (pq.size())
+    {
+        ll cost, node;
+        tie(cost, node) = pq.top();
+        pq.pop();
 
-	int ans = 0;
-	auto solve = [&](int ind)
-	{
-		int p = 0;
-		rep(k, ind)
-		{
-			while (p && lcps[q[p - 1]] > lcps[k])
-			{
-				next[q[--p]] = k;
-			}
-			q[p++] = k;
-		}
-		while (p) next[q[--p]] = ind;
+        if (seen[node]) continue;
+        seen[node] = true;
 
-		per(k, ind)
-		{
-			while (p && lcps[q[p - 1]] > lcps[k])
-			{
-				next[q[--p]] -= k;
-			}
-			q[p++] = k;
-		}
-		while (p) next[q[--p]] -= -1;
+        bool heavyNode = mustVisit.test(node);
+        if (heavyNode)
+        {
+            if (cost < costiest)
+            {
+                costiest = cost;
+                deepestNode = node;
+            }
+        }
 
-		rep(k, ind)
-		{
-			ans = max(ans, lcps[k] * next[k]);
-		}
-	};
+        repe(adj, edges[node])
+        {
+            pq.emplace(cost+adj.first, adj.second);
+        }
+    }
 
-	repp(l, 1, n)
-	{
-		rep(i, n) lcps[i] = 0;
-		int k = l;
-		int ind = 0;
-		while (k + l < n)
-		{
-			int u = 0;
-			int its = 0;
-			while (s[k + u] == s[k + l + u] && its < l) u++, its++;
+    return deepestNode;
+}
 
-			lcps[ind++] = u;
-			k += l;
-		}
-		solve(ind);
+void findVaild(vvp2& edges, set<ll>& ans, bitset<maxN>& mustVisit, ll n, ll start) {
+    priority_queue<tuple<int,int,int>> pq;
+    pq.emplace(0, -(ll)mustVisit.count(), start);
+    vi seen(n);
 
-		for (int j = l - 1; j >= 0; j--)
-		{
-			int k = j;
-			int ind2 = 0;
-			while (k + l < n)
-			{
-				if (s[k] == s[k + l])
-					lcps[ind2] = min(lcps[ind2] + 1, l);
-				else
-					lcps[ind2] = 0;
-				ind2++;
-				k += l;
-			}
+    while (pq.size())
+    {
+        ll cost, n_left, node;
+        tie(cost, n_left, node) = pq.top();
+        pq.pop();
 
-			k = j;
-			int ind3 = 0;
-			while (k + l * 2 <= n)
-			{
-				ind3++;
-				k += l;
-			}
+        if (seen[node]) continue;
+        seen[node] = true;
 
-			solve(ind3);
-		}
-	}
-	cout << ans << "\n";
+        if (mustVisit.test(node))
+        {
+            n_left++;
+        }
 
+        if (n_left==0) ans.insert(node);
 
+        repe(adj, edges[node])
+        {
+            pq.emplace(cost + adj.first, n_left, adj.second);
+        }
+    }
+}
 
-	return 0;
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+
+    ll n, m, k;
+    cin >> n >> m >> k;
+
+    vvp2 edges(n);
+
+    vi mustVisit(k);
+    bitset<maxN> seenAxel;
+    rep(i, k)
+    {
+        ll v;
+        cin >> v;
+        v--;
+        seenAxel.set(v);
+        mustVisit[i] = v;
+    }
+
+    rep(i,m)
+    {
+        ll a, b, w;
+        cin >> a >> b >> w;
+        a--;
+        b--;
+        edges[a].emplace_back(-w, b);
+        edges[b].emplace_back(-w, a);
+    }
+
+    ll deepestLeft = getfurthestnode(edges, seenAxel, n, mustVisit[0]);
+    ll deepestRight = getfurthestnode(edges, seenAxel, n, deepestLeft);
+
+    set<ll> ans;
+
+    findVaild(edges, ans, seenAxel, n, deepestLeft);
+    findVaild(edges, ans, seenAxel, n, deepestRight);
+
+    cout << sz(ans) << "\n";
+
+    repe(y,ans) {
+        cout << y + 1 << " ";
+    }
+
+    return 0;
 }
--- po/pocamp19/tradreklam.cpp
+++ po/pocamp19/tradreklam.cpp
@@ -1,101 +1,22 @@
-#undef _GLIBCXX_DEBUG                // disable run-time bound checking, etc
-#pragma GCC optimize("Ofast,inline") // Ofast = O3,fast-math,allow-store-data-races,no-protect-parens
-#pragma GCC optimize ("unroll-loops")
-
-#pragma GCC target("bmi,bmi2,lzcnt,popcnt")                      // bit manipulation
-#pragma GCC target("movbe")                                      // byte swap
-#pragma GCC target("aes,pclmul,rdrnd")                           // encryption
-#pragma GCC target("avx,avx2,f16c,fma,sse3,ssse3,sse4.1,sse4.2") // SIMD
-
 #include <bits/stdc++.h>
-//#include <bits/extc++.h>
-
 using namespace std;
 
-#define enablell 0
+using ll = long long;
+using vi = vector<ll>;
+using vvi = vector<vi>;
+using p2 = pair<ll, ll>;
+const ll inf = 1e18;
 
-#define ll long long
-#if enablell
-#define int ll
-#define inf LLONG_MAX
-#define float double
-#else
-#define inf int(2e9)
-#endif
-#define vi vector<int>
-#define vvi vector<vi>
-#define vvvi vector<vvi>
-#define vvvvi vector<vvvi>
-#define vb vector<bool>
-#define vvb vector<vb>
-#define vvvb vector<vvb>
-#define p2 pair<int, int>
-#define vp2 vector<p2>
-#define vvp2 vector<vp2>
-#define vvvp2 vector<vvp2>
-#define p3 tuple<int,int,int>
-#define vp3 vector<p3>
-#define vvp3 vector<vp3>
-#define vvvp3 vector<vvp3>
-#define p4 tuple<int,int,int,int>
-#define vp4 vector<p4>
+#define rep(i,n) for (ll i = 0; i < (n); i++)
+#define repp(i,a,n) for (ll i = (a); i < (n); i++)
+#define repe(i, arr) for (auto& i : arr)
+#define all(x) begin(x),end(x)
+#define sz(x) ((ll)(x).size())
 
-#define read(a) cin >> a
-#define read2(a,b) cin >> a >> b
-#define read3(a,b,c) cin >> a >> b >> c
-#define write(a) cout << (a) << "\n"
-#define quit cout << endl; _Exit(0);
-#define dread(type, a) type a; cin >> a
-#define dread2(type, a, b) dread(type, a); dread(type, b)
-#define dread3(type, a, b, c) dread2(type, a, b); dread(type, c)
-#define dread4(type, a, b, c, d) dread3(type, a, b, c); dread(type, d)
-#define dread5(type, a, b, c, d, e) dread4(type, a, b, c, d); dread(type, e)
-#ifdef _DEBUG
-#define noop cout << "";
-#define deb __debugbreak();
-#define debassert(expr) if (!(expr)) deb;
-#define debif(expr) if(expr) deb;
-#else
-#define noop ;
-#define deb ;
-#define debassert(expr) ;
-#define debif(expr) ;
-#endif
-
-#define rep(i, high) for (int i = 0; i < high; i++)
-#define repp(i, low, high) for (int i = low; i < high; i++)
-#define repe(i, container) for (auto& i : container)
-#define per(i, high) for (int i = high-1; i >= 0; i--)
-#define perr(i, low, high) for (int i = high-1; i >= low; i--)
-
-#define readvector(type, name, size) vector<type> name(size); rep(i,size) {dread(type,temp); name[i]=temp;}
-#define all(a) begin(a),end(a)
-#define setcontains(set, x) (set.find(x) != set.end())
-#define stringcontains(str, x) (str.find(x) != string::npos)
-#define within(a, b, c, d) (a >= 0 && a < b && c >= 0 && c < d)
-#define sz(container) ((int)container.size())
-#define mp(a,b) (make_pair(a,b))
-#define first(a) (*begin(a))
-#define indexpair(p, i) ((i==0)?p.first:p.second)
-#define chmax(a,b) ((a)=max((a),b))
-#define chmin(a,b) ((a)=min((a),b))
-
-#define ceildiv(x,y) ((x + y - 1) / y)
-#define fract(a) (a-floor(a))
-
-auto Start = chrono::high_resolution_clock::now();
-#define elapsedmillis() (chrono::duration_cast<chrono::milliseconds>(chrono::high_resolution_clock::now() - Start).count())
-#define rununtil(time) if (elapsedmillis() >= time) break;
-
-inline void fast() { ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL); }
-template <typename T, typename U> inline void operator+=(std::pair<T, U>& l, const std::pair<T, U>& r) { l = { l.first + r.first,l.second + r.second }; }
-template <typename T> inline int sgn(T val) { return (T(0) < val) - (val < T(0)); }
-template <typename Out> inline void split(const string& s, char delim, Out result) { istringstream iss(s); string item; while (getline(iss, item, delim)) { *result++ = item; } }
-inline vector<string> split(const string& s, char delim) { vector<string> elems; split(s, delim, back_inserter(elems)); return elems; }
-
-int fillcities(vp2& cities, vvp2& edges, vi& populations, int node, int par)
-{
-    int ret = 0;
+using vp2 = vector<p2>;
+using vvp2 = vector<vp2>;
+ll fillcities(vp2& cities, vvp2& edges, vi& populations, ll node, ll par) {
+    ll ret = 0;
 
     repe(edge, edges[node])
     {
@@ -113,14 +34,13 @@ int fillcities(vp2& cities, vvp2& edges, vi& populations, int node, int par)
 
 // Cities: first cost, second value
 // For each cost of each node, best is to either take this node or subset of children nodes
-void best(vp2& cities, vvp2& edges, vi& dp, int node, int par)
+void best(vp2& cities, vvp2& edges, vi& dp, ll node, ll par)
 {
     vi dp2(dp.size(), 0);
 
-
     rep(i, dp.size())
     {
-        int newV = 0;
+        ll newV = 0;
         if (cities[node].first <= i)
         {
             newV = cities[node].second + dp[i - cities[node].first];
@@ -140,15 +60,11 @@ void best(vp2& cities, vvp2& edges, vi& dp, int node, int par)
 
 }
 
-int32_t main()
-{
-    fast();
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
 
-#if 0
-    ifstream cin("C:\\Users\\Matis\\source\\repos\\Comp prog\\x64\\Debug\\in.txt");
-#endif
-
-    dread2(int, n, b);
+    ll n, b;
+    cin >> n >> b;
 
     vvp2 edges(n);
 
@@ -157,13 +73,13 @@ int32_t main()
 
     rep(i, n - 1)
     {
-        dread3(int, a, b, c);
+        ll a, b, c;
+        cin >> a >> b >> c;
         a--;
         b--;
         edges[a].emplace_back(b, c);
         edges[b].emplace_back(a, c);
     }
-
 
     // Cost, value
     vp2 cities(n);
@@ -176,5 +92,5 @@ int32_t main()
 
     cout << *max_element(all(dp));
 
-    quit;
+    return 0;
 }
